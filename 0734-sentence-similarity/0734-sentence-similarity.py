5db48ad17613ccf609e2c5295cb22d82a5aeb811
Seoyoung Kim <70043001+syoungk7@users.noreply.github.com>
2023-05-18 21:30:12 -0400

    Time: 54 ms (59.68%), Space: 16.4 MB (32.26%) - LeetHub

---
+++ 0734-sentence-similarity/0734-sentence-similarity.py
@@ -0,0 +1,17 @@
+class Solution:
+    def areSentencesSimilar(self, sentence1: List[str], sentence2: List[str], similarPairs: List[List[str]]) -> bool:
+        if len(sentence1) != len(sentence2):
+            return False
+        
+        if len(sentence1) == len(sentence2):
+            for i, j in zip(sentence1, sentence2):
+                #print([i, j], [j, i])
+                if i == j:
+                    pass
+                elif [i, j] in similarPairs or [j, i] in similarPairs :
+                    pass
+                else:
+                    return False
+        return True
+                
+        
