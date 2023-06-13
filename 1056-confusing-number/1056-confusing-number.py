d24c36477c5578ac239b0ec3b8533603237d9908
Seoyoung Kim <70043001+syoungk7@users.noreply.github.com>
2023-06-12 20:51:34 -0400

    Time: 45 ms (51.40%), Space: 16.4 MB (21.36%) - LeetHub

---
+++ 1056-confusing-number/1056-confusing-number.py
@@ -0,0 +1,16 @@
+class Solution:
+    def confusingNumber(self, n: int) -> bool:
+        lst = [2, 3, 4, 5, 7]
+        new = []
+        for num in str(n):
+            if int(num) in lst:
+                return False
+            else:
+                if num == '9': num = '6' 
+                elif num == '6': num = '9' 
+                new.append(num)
+        print(new, ''.join(new[::-1]))
+        if int(n) != int(''.join(new[::-1])):
+            return True
+        else:
+            return False
