63719a7df242bc4a76aaf6970521701bdbe3d3da
Seoyoung Kim <70043001+syoungk7@users.noreply.github.com>
2023-06-14 23:46:06 -0400

    Time: 1103 ms (45.51%), Space: 154.1 MB (12.58%) - LeetHub

--- 1057-campus-bikes/1057-campus-bikes.py
+++ 1057-campus-bikes/1057-campus-bikes.py
@@ -7,12 +7,12 @@ class Solution:
         for w, worker in enumerate(workers):
             for b, bike in enumerate(bikes): 
                 dist = abs(bike[0] - worker[0]) + abs(bike[1] - worker[1])
-                totals.append((dist, w, b))
+                totals.append((w, b, dist))
         #print(totals)
-        totals.sort()
+        totals.sort(key=lambda a: a[2])
         #print(totals)
 
-        for _, w, b in totals:
+        for w, b, dist in totals:
             if answer[w] == -1 and b not in used:
                 answer[w] = b
                 used.append(b)
