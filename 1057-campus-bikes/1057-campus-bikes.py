ec94a80c043056380b2e326b59571447931fe051
Seoyoung Kim <70043001+syoungk7@users.noreply.github.com>
2023-06-15 00:11:35 -0400

    Time: 1059 ms (47.31%), Space: 154.2 MB (12.58%) - LeetHub

--- 1057-campus-bikes/1057-campus-bikes.py
+++ 1057-campus-bikes/1057-campus-bikes.py
@@ -2,15 +2,13 @@ class Solution:
     def assignBikes(self, workers: List[List[int]], bikes: List[List[int]]) -> List[int]:
         totals, used = [], []
         answer = [-1] * len(workers)
-        pre = -1
         
         for w, worker in enumerate(workers):
             for b, bike in enumerate(bikes): 
                 dist = abs(bike[0] - worker[0]) + abs(bike[1] - worker[1])
                 totals.append((w, b, dist))
-        #print(totals)
+
         totals.sort(key=lambda a: a[2])
-        #print(totals)
 
         for w, b, dist in totals:
             if answer[w] == -1 and b not in used:
