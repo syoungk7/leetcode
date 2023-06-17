b2d07d064459b6a5bc5192e9812ce49f27dd6afd
Seoyoung Kim <70043001+syoungk7@users.noreply.github.com>
2023-06-16 21:36:56 -0400

    Time: 309 ms (23.30%), Space: 23.1 MB (38.64%) - LeetHub

--- 1060-missing-element-in-sorted-array/1060-missing-element-in-sorted-array.py
+++ 1060-missing-element-in-sorted-array/1060-missing-element-in-sorted-array.py
@@ -3,10 +3,8 @@ class Solution:
         
         for idx in range(len(nums)-1):
             missing = nums[idx+1] - nums[idx] -1
-            print(idx, missing, k)
             if missing < k:
                 k = k - missing
-                print('new k:', k)
                 continue
             else:
                 return nums[idx] + k
