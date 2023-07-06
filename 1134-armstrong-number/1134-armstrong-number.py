ca5df72e69bfbc317461d935bb4f341c23c56b3c
Seoyoung Kim <70043001+syoungk7@users.noreply.github.com>
2023-07-05 20:51:24 -0400

    Time: 35 ms (96.31%), Space: 16.3 MB (23.83%) - LeetHub

--- 1134-armstrong-number/1134-armstrong-number.py
+++ 1134-armstrong-number/1134-armstrong-number.py
@@ -1,11 +1,9 @@
 class Solution:
     def isArmstrong(self, n: int) -> bool:
-        if type(n) is not int:
-            return False
-
         str_n = str(n)
         len_n = len(str_n)
         summa = 0
+
         for i in str_n:
             summa += int(i)** len_n
         
