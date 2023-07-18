a4bb2007e0e8e4e675e413ef53156ec80ea828f7
Seoyoung Kim <70043001+syoungk7@users.noreply.github.com>
2023-07-18 13:58:09 -0400

    Time: 51 ms (37.13%), Space: 16.2 MB (72.15%) - LeetHub

---
+++ 1180-count-substrings-with-only-one-distinct-letter/1180-count-substrings-with-only-one-distinct-letter.py
@@ -0,0 +1,28 @@
+class Solution:
+    def countLetters(self, s: str) -> int:
+        lst = []
+        letter = '0'
+        intial = s[0]
+        for idx, val in enumerate(s):
+            if s[idx] == letter[0]:
+                letter = letter + s[idx]
+            else:
+                lst.append(letter)
+                letter = ''
+                letter = s[idx]
+        lst.append(letter)
+        lst.pop(0)
+        
+        summ = 0
+        for i in lst:
+            if len(i) == 1:
+                summ += 1
+                print('if', summ)
+            else:
+                count = 1
+                while count < len(i):
+                    summ += count
+                    count += 1
+                summ += count
+                print('wh', summ)
+        return summ
