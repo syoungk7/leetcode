7c8757ff37a89b483706c9ef4b2f013e0b75d696
Seoyoung Kim <70043001+syoungk7@users.noreply.github.com>
2023-07-19 21:07:48 -0400

    Time: 330 ms (47.37%), Space: 24.2 MB (8.92%) - LeetHub

--- 1059-all-paths-from-source-lead-to-destination/1059-all-paths-from-source-lead-to-destination.py
+++ 1059-all-paths-from-source-lead-to-destination/1059-all-paths-from-source-lead-to-destination.py
@@ -1,23 +1,23 @@
 class Solution:
     def leadsToDestination(self, n: int, edges: List[List[int]], source: int, destination: int) -> bool:
         path = collections.defaultdict(set)
-        lst = [None for i in range(n)]
+        lst = [None] * n
         
         for s, dest in edges:
             path[s].add(dest)
         #print(path) 
-        Test, Pass = 1, 2
+        
         def find_path(s):
             if lst[s]:
-                return lst[s] == Test
+                return lst[s] == 1
             if not path[s]:
                 return s == destination
              
-            lst[s] = Pass
+            lst[s] = 2
             for d in path[s]:
                 if not find_path(d):
                     return False
-            lst[s] = Test
+            lst[s] = 1
             return True
 
         return find_path(source)
