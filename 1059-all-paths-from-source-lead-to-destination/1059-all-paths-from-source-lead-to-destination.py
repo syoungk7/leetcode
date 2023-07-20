8a48b65d816dbd0687748dcf9a0252ef8704f1c8
Seoyoung Kim <70043001+syoungk7@users.noreply.github.com>
2023-07-19 20:56:52 -0400

    Time: 316 ms (86.27%), Space: 24.2 MB (8.92%) - LeetHub

--- 1059-all-paths-from-source-lead-to-destination/1059-all-paths-from-source-lead-to-destination.py
+++ 1059-all-paths-from-source-lead-to-destination/1059-all-paths-from-source-lead-to-destination.py
@@ -1,25 +1,48 @@
 class Solution:
     def leadsToDestination(self, n: int, edges: List[List[int]], source: int, destination: int) -> bool:
-        path = collections.defaultdict(set)
-        lst = [0 for i in range(n)]
+#         path = collections.defaultdict(set)
+#         lst = [None for i in range(n)]
+#         print(lst)
         
-        for s, dest in edges:
-            path[s].add(dest)
-        print(path) 
+#         for s, dest in edges:
+#             path[s].add(dest)
+#         print(path) 
         
-        def find_path(s):
-            if len(path[s]) == 0:
-                return s == destination
-            elif lst[s] == 1:
-                return False
-            elif lst[s] == 2:
-                return True
-            else:
-                lst[s] = 1
-                for j in path[s]:
-                    if not find_path(j):
-                        return False
-                lst[s] = 2
-                return True
+#         def find_path(s):
+#             if lst[s]:
+#                 return lst[s] == 1
+#             if not path(s):
+#                 return s == destination
+             
+#             lst[s] = 2
+#             for d in path[s]:
+#                 if not find_path(d):
+#                     return False
+#             lst[s] = 1
+#             return True
 
-        return find_path(source)
+#         return find_path(source)
+        adj_list = collections.defaultdict(set)
+        for a, b in edges:
+            adj_list[a].add(b)
+
+        BLACK = 1  # fully processed nodes
+        GRAY = 2  # nodes being processed
+        states = [None] * n
+        def recurr(node):
+            if states[node]:
+                return states[node] == BLACK
+
+            if not adj_list[node]:
+                return node == destination
+
+            states[node] = GRAY
+
+            for nei in adj_list[node]:
+                if not recurr(nei):
+                    return False
+
+            states[node] = BLACK
+            return True
+
+        return recurr(source)
