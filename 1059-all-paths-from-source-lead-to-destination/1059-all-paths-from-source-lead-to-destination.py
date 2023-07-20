2e7f36aef82d0aa08d4c0be8baf926a21ad24ea1
Seoyoung Kim <70043001+syoungk7@users.noreply.github.com>
2023-07-19 21:04:29 -0400

    Time: 331 ms (44.16%), Space: 24 MB (12.59%) - LeetHub

--- 1059-all-paths-from-source-lead-to-destination/1059-all-paths-from-source-lead-to-destination.py
+++ 1059-all-paths-from-source-lead-to-destination/1059-all-paths-from-source-lead-to-destination.py
@@ -1,48 +1,23 @@
 class Solution:
     def leadsToDestination(self, n: int, edges: List[List[int]], source: int, destination: int) -> bool:
-#         path = collections.defaultdict(set)
-#         lst = [None for i in range(n)]
-#         print(lst)
+        path = collections.defaultdict(set)
+        lst = [None for i in range(n)]
         
-#         for s, dest in edges:
-#             path[s].add(dest)
-#         print(path) 
+        for s, dest in edges:
+            path[s].add(dest)
+        #print(path) 
         
-#         def find_path(s):
-#             if lst[s]:
-#                 return lst[s] == 1
-#             if not path(s):
-#                 return s == destination
+        def find_path(s):
+            if lst[s]:
+                return lst[s] == 1
+            if not path[s]:
+                return s == destination
              
-#             lst[s] = 2
-#             for d in path[s]:
-#                 if not find_path(d):
-#                     return False
-#             lst[s] = 1
-#             return True
-
-#         return find_path(source)
-        adj_list = collections.defaultdict(set)
-        for a, b in edges:
-            adj_list[a].add(b)
-
-        BLACK = 1  # fully processed nodes
-        GRAY = 2  # nodes being processed
-        states = [None] * n
-        def recurr(node):
-            if states[node]:
-                return states[node] == BLACK
-
-            if not adj_list[node]:
-                return node == destination
-
-            states[node] = GRAY
-
-            for nei in adj_list[node]:
-                if not recurr(nei):
+            lst[s] = 2
+            for d in path[s]:
+                if not find_path(d):
                     return False
-
-            states[node] = BLACK
+            lst[s] = 1
             return True
 
-        return recurr(source)
+        return find_path(source)
