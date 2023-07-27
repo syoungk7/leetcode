49a71ecdd583db4b2cfd659598ab50b272858b06
Seoyoung Kim <70043001+syoungk7@users.noreply.github.com>
2023-07-27 19:01:19 -0400

    Time: 49 ms (96.62%), Space: 16.5 MB (29.11%) - LeetHub

--- 1228-missing-number-in-arithmetic-progression/1228-missing-number-in-arithmetic-progression.py
+++ 1228-missing-number-in-arithmetic-progression/1228-missing-number-in-arithmetic-progression.py
@@ -1,18 +1,26 @@
 class Solution:
     def missingNumber(self, arr: List[int]) -> int:
-        m, pre = len(arr), 0
-        arr = sorted(arr)
+        diff = int((arr[-1] - arr[0]) / len(arr))
+        if diff == 0: return arr[0]
         
-        if len(set(arr)) == 1:
-            return arr[0]
+        lst = list(range(arr[0], arr[-1]+diff, diff))
+        a = list(set(lst) - set(arr))
+        return a[0]
         
-        for i in range(round(m/2)+1):
-            left = arr[i+1] - arr[i]
-            right = arr[m-(i+1)] - arr[m-(i+2)]
-            print(left, right)
-            if left > right: return int(arr[i] + left/2)
-            elif left < right: return int(arr[m-(i+2)] + right/2)
+        
+#         m, pre = len(arr), 0
+#         arr = sorted(arr)
+        
+#         if len(set(arr)) == 1:
+#             return arr[0]
+        
+#         for i in range(round(m/2)+1):
+#             left = arr[i+1] - arr[i]
+#             right = arr[m-(i+1)] - arr[m-(i+2)]
+#             print(left, right)
+#             if left > right: return int(arr[i] + left/2)
+#             elif left < right: return int(arr[m-(i+2)] + right/2)
             
-            if pre != 0 and pre != left: return int(arr[i] + left/2)
+#             if pre != 0 and pre != left: return int(arr[i] + left/2)
             
-            pre = left
+#             pre = left
