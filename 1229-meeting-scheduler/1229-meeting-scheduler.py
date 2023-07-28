18dc91b8b83aaf26db1faa0af4bd0da9e0b304ed
Seoyoung Kim <70043001+syoungk7@users.noreply.github.com>
2023-07-28 00:42:30 -0400

    Time: 508 ms (93.23%), Space: 24.9 MB (14.58%) - LeetHub

--- 1229-meeting-scheduler/1229-meeting-scheduler.py
+++ 1229-meeting-scheduler/1229-meeting-scheduler.py
@@ -1,8 +1,7 @@
 class Solution:
     def minAvailableDuration(self, slots1: List[List[int]], slots2: List[List[int]], duration: int) -> List[int]:
-        slots1 = sorted(slots1)
-        slots2 = sorted(slots2)
-        
+        # ref: solution
+        slots1, slots2 = sorted(slots1), sorted(slots2) 
         s1, s2 = 0, 0
         
         while s1 < len(slots1) and s2 < len(slots2):
@@ -11,10 +10,10 @@ class Solution:
             if min(i[1], j[1]) - max(i[0], j[0]) >= duration:
                 return [max(i[0], j[0]), max(i[0], j[0]) + duration]       
 
-            if j[1] < i[1]:
-                s2 += 1
-            else:
-                s1 += 1
+            if j[1] < i[1]: s2 += 1
+            else: s1 += 1
+
+        return []
 
 ## Time Limit Exceeded        
 #         for i in sorted(slots2):
@@ -22,10 +21,5 @@ class Solution:
 #                 if min(i[1], j[1]) - max(i[0], j[0]) >= duration:
 #                     return [max(i[0], j[0]), max(i[0], j[0]) + duration]
                 
-#                 if j[1] < i[1]:
-#                     continue
-#                 elif j[1] > i[1]:
-#                     break
-                    
-        return []
-                
+#                 if j[1] < i[1]: continue
+#                 elif j[1] > i[1]: break
