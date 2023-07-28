b668975be6428ced55b5340345c747351ab021cf
Seoyoung Kim <70043001+syoungk7@users.noreply.github.com>
2023-07-27 22:41:08 -0400

    Time: 268 ms (12.49%), Space: 22.9 MB (47.40%) - LeetHub

--- 1214-two-sum-bsts/1214-two-sum-bsts.py
+++ 1214-two-sum-bsts/1214-two-sum-bsts.py
@@ -6,6 +6,8 @@
 #         self.right = right
 class Solution:
     def twoSumBSTs(self, root1: Optional[TreeNode], root2: Optional[TreeNode], target: int) -> bool:
+        self.ans=False
+        
         # from solution
         def dfs(curr_node, node_list):
             if not curr_node:
@@ -13,15 +15,22 @@ class Solution:
             node_list.append(curr_node.val)
             dfs(curr_node.left, node_list)
             dfs(curr_node.right, node_list)
-        
-        root1_lst, root2_lst = [], []
+            
+        def dfs_2(curr_node):
+            if not curr_node:
+                return
+            print(curr_node.val)
+            if curr_node.val in sub_lst:
+                self.ans=True
+            dfs_2(curr_node.left)
+            dfs_2(curr_node.right)
+
+        root1_lst, sub_lst = [], []
         dfs(root1, root1_lst)
-        dfs(root2, root2_lst)
-        
         for i in root1_lst:
-            subt = target-i
-            
-            if subt in root2_lst: 
-                return True
-            
-        return False
+            sub_lst.append(target-i)
+        print(sub_lst)
+        
+        dfs_2(root2)
+        
+        return self.ans 
