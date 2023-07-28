af9c8ecbe14fa5f295aa2eaf3a2e6ced18e112ef
Seoyoung Kim <70043001+syoungk7@users.noreply.github.com>
2023-07-27 22:47:12 -0400

    Time: 260 ms (13.08%), Space: 22.7 MB (63.85%) - LeetHub

--- 1214-two-sum-bsts/1214-two-sum-bsts.py
+++ 1214-two-sum-bsts/1214-two-sum-bsts.py
@@ -7,30 +7,24 @@
 class Solution:
     def twoSumBSTs(self, root1: Optional[TreeNode], root2: Optional[TreeNode], target: int) -> bool:
         self.ans=False
-        
-        # from solution
-        def dfs(curr_node, node_list):
-            if not curr_node:
-                return
-            node_list.append(curr_node.val)
-            dfs(curr_node.left, node_list)
-            dfs(curr_node.right, node_list)
-            
-        def dfs_2(curr_node):
-            if not curr_node:
-                return
-            print(curr_node.val)
-            if curr_node.val in sub_lst:
-                self.ans=True
-            dfs_2(curr_node.left)
-            dfs_2(curr_node.right)
+        root1_lst = []
 
-        root1_lst, sub_lst = [], []
+        # from solution
+        def dfs(node, node_list):
+            if not node: return
+            node_list.append(node.val)
+            dfs(node.left, node_list)
+            dfs(node.right, node_list)
+            
+        def dfs_2(node):
+            if not node: return
+            print(target - node.val)
+            if target - node.val in root1_lst:
+                self.ans=True
+            dfs_2(node.left)
+            dfs_2(node.right)
+
         dfs(root1, root1_lst)
-        for i in root1_lst:
-            sub_lst.append(target-i)
-        print(sub_lst)
-        
         dfs_2(root2)
         
         return self.ans 
