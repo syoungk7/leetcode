6b4b24f041712664fa87e3892e8db1ed08e9f518
Seoyoung Kim <70043001+syoungk7@users.noreply.github.com>
2023-08-02 22:12:34 -0400

    Time: 204 ms (23.04%), Space: 22.5 MB (32.84%) - LeetHub

--- 1245-tree-diameter/1245-tree-diameter.py
+++ 1245-tree-diameter/1245-tree-diameter.py
@@ -1,56 +1,33 @@
 class Solution:
     def treeDiameter(self, edges: List[List[int]]) -> int:
 
-#         # edges to dict 
-#         # consider both case right to left and left to right
-#         tree = collections.defaultdict(set)
-#         for left, right in edges:
-#             tree[left].add(right)
-#             tree[right].add(left)        
-#         #print(tree, tree[0])
-            
-#         # target: the longest path
-#         def diameter(node, diam = 0):
-#             passed = set(node)
-            
-#             while node:
-#                 path = []
-#                 for i in node:
-#                     print(tree[i])
-#                     for j in tree[i]:
-#                         if j not in passed:
-#                             path.append(j)
-#                             passed.add(j)
-#                 node = path
-#                 diam += 1
-#                 print(node, passed, diam)
-#             return diam
+        # edges to dict 
+        # consider both case right to left and left to right
+        tree = collections.defaultdict(list)
+        for left, right in edges:
+            tree[left].append(right)
+            tree[right].append(left)        
+        #print(tree, tree[0])
         
-#         return diameter([0], 0)
-
-        graph = collections.defaultdict(list)
-        for a, b in edges:
-            graph[a].append(b)
-            graph[b].append(a)
-        #visited = set()
-        self.ans = 0
-        def dfs(node, pre):
+        # target: the longest path
+        def diameter(node, node2):
+            diam = 0
+            level1, level2 = 0, 0
             
-            #visited.add(node)
-            d1, d2 = 0, 0
-            for nei in graph[node]:
-                #if nei not in visited:
-                if nei != pre:
-                    d = dfs(nei, node)
-                    if d > d1:
-                        d2 = d1
-                        d1 = d
+            for n in tree[node]:
+                if n != node2:
+                    dd, ll = diameter(n, node)
+                    diam = max(diam, dd)
+                    print(diam, ll, level1, level2)
+                    if ll > level2:
+                        level2 = ll
+                    
+                        if level2 > level1:
+                            level2, level1 = level1, level2
+            print(diam, level1, level2)
+            return max(diam, level1+level2), max(level1+1, level2+1)
 
-                    elif d > d2:
-                        d2 = d
-
-            self.ans = max(self.ans, d1+d2)
-            return 1 + max(d1, d2)
-
-        dfs(0, -1)
-        return self.ans
+        
+        
+        return diameter(0, None)[0]
+            
