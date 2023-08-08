6d31d7fc04a397ead6ec8be13f25ade1fa884695
Seoyoung Kim <70043001+syoungk7@users.noreply.github.com>
2023-08-07 21:04:55 -0400

    Time: 57 ms (30.52%), Space: 16.4 MB (40.47%) - LeetHub

--- 1426-counting-elements/1426-counting-elements.py
+++ 1426-counting-elements/1426-counting-elements.py
@@ -2,10 +2,8 @@ class Solution:
     def countElements(self, arr: List[int]) -> int:
         count = 0
         arr = sorted(arr)
-# #         if arr is None:
-# #             return 0
-        print(len(arr))
-        for i in range(0, len(arr)):
-            if arr[i]+1 in set(arr):
+
+        for i in range(len(arr)):
+            if arr[i]+1 in arr:
                 count += 1
         return count
