65de4068370737fb26fd453fe784fda8aa422315
Seoyoung Kim <70043001+syoungk7@users.noreply.github.com>
2023-08-13 03:45:13 -0400

    Time: 108 ms (5.17%), Space: 16.7 MB (6.20%) - LeetHub

--- 1099-two-sum-less-than-k/1099-two-sum-less-than-k.py
+++ 1099-two-sum-less-than-k/1099-two-sum-less-than-k.py
@@ -1,14 +1,41 @@
 class Solution:
     def twoSumLessThanK(self, nums: List[int], k: int) -> int:
-        import itertools
-        lst = []
-        for i in itertools.permutations(nums, 2):
-            if sum(i) < k:
-                lst.append(sum(i))
-        if len(lst) == 0:
-            return -1
-        else:
-            new = sorted(lst)
+        summ = []
+        if len(nums) < 2 : return -1
+
+        for i in range(len(nums)):
+            for j in range(len(nums)):
+                if i == j: continue
+                
+                summ.append(nums[i] + nums[j])
+
+        if min(summ) > k: return -1
+
+        return max(m for m in summ if m < k)
         
-        return new[-1]
+#         for i, val in enumerate(sorted(nums, reverse = True)):
+#             if val > k:
+#                 continue
+#             else:
+                
+#             pre = summ
+#             summ += val
+            
+#             if summ > k:
+#                 if i == 1 or i == 0: return -1
+#                 else: return pre
+#         return -1
+        
+        
+#         import itertools
+#         lst = []
+#         for i in itertools.permutations(nums, 2):
+#             if sum(i) < k:
+#                 lst.append(sum(i))
+#         if len(lst) == 0:
+#             return -1
+#         else:
+#             new = sorted(lst)
+        
+#         return new[-1]
         
