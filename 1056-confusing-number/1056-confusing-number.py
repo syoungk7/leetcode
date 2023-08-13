1ca360265bd4cdee0b842fd8283adfa9d8788089
Seoyoung Kim <70043001+syoungk7@users.noreply.github.com>
2023-08-13 02:15:33 -0400

    Time: 49 ms (27.93%), Space: 16.4 MB (36.11%) - LeetHub

--- 1056-confusing-number/1056-confusing-number.py
+++ 1056-confusing-number/1056-confusing-number.py
@@ -1,17 +1,19 @@
 class Solution:
     def confusingNumber(self, n: int) -> bool:
-        new_map = {'0': '0', '1': '1', '6': '9', '8': '8', '9': '6'}
+        new_map = {'6': '9', '9': '6'}
         new = ""
         
         for i in str(n):
-            if i not in new_map: return False
-            new += new_map[i]
+            if i in '23457': return False
+            if i == '9' or i == '6': new += new_map[i]
+            else: new += i
+            print(new)
 
         if int(n) != int(new[::-1]): return True
         
         return False
             
-
+#         num = []
 #         for num in str(n):
 #             if num in '23457': return False
 #             if num in '96':
@@ -20,18 +22,3 @@ class Solution:
 #             new.append(num)
 #         if int(n) != int(''.join(new[::-1])): return True
 #         return False
-    
-#     class Solution:
-#     def confusingNumber(self, n: int) -> bool:
-#         digit_rotation_map = {0: 0, 1: 1, 6: 9, 8: 8, 9: 6}
-#         num_str = str(n)
-#         new_num_str = ""
-#         for digit in num_str[::-1]:
-#             if int(digit) not in digit_rotation_map:
-#                 return False
-            
-#             new_num_str += str(digit_rotation_map[int(digit)])
-
-#         if n == int(new_num_str):
-#             return False
-#         return True
