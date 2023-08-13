3cdcf2401a8fc86b6df84d4bfe0428d6ad9d96b7
Seoyoung Kim <70043001+syoungk7@users.noreply.github.com>
2023-08-13 04:32:47 -0400

    Time: 45 ms (97.00%), Space: 16.5 MB (55.31%) - LeetHub

--- 1133-largest-unique-number/1133-largest-unique-number.py
+++ 1133-largest-unique-number/1133-largest-unique-number.py
@@ -1,14 +1,14 @@
 class Solution:
     def largestUniqueNumber(self, nums: List[int]) -> int:
-        uniq, delt = [], []
+        uniq, delt = set(), set()
         
         for i in sorted(nums, reverse=True):
             if i not in uniq and i not in delt:
-                uniq.append(i)
+                uniq.add(i)
             elif i in uniq and i not in delt:
                 uniq.remove(i)
-                delt.append(i)
+                delt.add(i)
         
         if len(uniq) == 0: return -1
         
-        return max(uniq)
+        return max(list(uniq))
