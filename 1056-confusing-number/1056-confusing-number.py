a9ba9a70f6b4270f85a8c5cc4817fe20c9c9c7a5
Seoyoung Kim <70043001+syoungk7@users.noreply.github.com>
2023-08-13 01:49:57 -0400

    Time: 37 ms (86.57%), Space: 16.2 MB (93.83%) - LeetHub

--- 1056-confusing-number/1056-confusing-number.py
+++ 1056-confusing-number/1056-confusing-number.py
@@ -3,9 +3,10 @@ class Solution:
         new = []
         for num in str(n):
             if num in '23457': return False
-
-            if num == '9': num = '6' 
-            elif num == '6': num = '9' 
+            
+            if num in '96':
+                if num == '9': num = '6' 
+                else: num = '9' 
             new.append(num)
 
         if int(n) != int(''.join(new[::-1])): return True
