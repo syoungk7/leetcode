c20f7cf15bba579e45d05702d8a9dd22677ff0cb
Seoyoung Kim <70043001+syoungk7@users.noreply.github.com>
2023-08-14 21:03:29 -0400

    Time: 194 ms (74.00%), Space: 21.7 MB (61.33%) - LeetHub

--- 1485-clone-binary-tree-with-random-pointer/1485-clone-binary-tree-with-random-pointer.py
+++ 1485-clone-binary-tree-with-random-pointer/1485-clone-binary-tree-with-random-pointer.py
@@ -13,8 +13,8 @@ class Solution:
         def dfs(o_node, cp):
             if not o_node: return None
             if o_node in cp: return cp[o_node]
-
-            new = NodeCopy(o_node.val)
+            else:
+                new = NodeCopy(o_node.val)
             cp[o_node] = new
 
             new.left = dfs(o_node.left, cp)
@@ -23,3 +23,53 @@ class Solution:
             return new
         
         return dfs(root, cp)
+    
+## ref solution
+
+# Recursive DFS
+
+#     def __init__(self):
+#         # track visited nodes to avoid cycles
+#         self.visited = dict()
+
+#     def copyRandomBinaryTree(self, root: "Node") -> "NodeCopy":
+#         if not root: return
+
+#         # check if already created and return
+#         if root in self.visited: return self.visited[root]
+
+#         # otherwise, create a node and remember to save it in visited
+#         node = NodeCopy(root.val, None, None, None)
+#         self.visited[root] = node
+
+#         # recurse on other pointers
+#         node.left = self.copyRandomBinaryTree(root.left)
+#         node.right = self.copyRandomBinaryTree(root.right)
+#         node.random = self.copyRandomBinaryTree(root.random)
+
+#         return node
+
+# Iterative DFS
+
+#     def copyRandomBinaryTree(self, root: "Node") -> "NodeCopy":
+#         if not root: return
+
+#         # track visited nodes to avoid cycles
+#         visited = collections.defaultdict(lambda: NodeCopy())
+#         visited[None] = None
+#         S = [root]
+
+#         while S:
+#             node = S.pop()
+
+#             # create old_node to new_node reference
+#             visited[node].val = node.val
+#             visited[node].left = visited[node.left]
+#             visited[node].right = visited[node.right]
+#             visited[node].random = visited[node.random]
+
+#             # explore children
+#             for child in (node.left, node.right):
+#                 if child: S.append(child)
+
+#         return visited[root]
