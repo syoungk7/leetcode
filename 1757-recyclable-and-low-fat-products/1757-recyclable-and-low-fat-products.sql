67028885bfb8c42c3d466b604e504b6ab887664f
Seoyoung Kim <70043001+syoungk7@users.noreply.github.com>
2023-08-15 09:52:23 -0400

    Time: 1677 ms (10.16%), Space: 0B (100.00%) - LeetHub

--- 1757-recyclable-and-low-fat-products/1757-recyclable-and-low-fat-products.sql
+++ 1757-recyclable-and-low-fat-products/1757-recyclable-and-low-fat-products.sql
@@ -1,2 +1,4 @@
-# Write your MySQL query statement below
-select product_id from Products where low_fats = 'Y' and recyclable = 'Y';
+/* Write your PL/SQL query statement below */
+select product_id 
+from Products 
+where low_fats = 'Y' and recyclable = 'Y';
