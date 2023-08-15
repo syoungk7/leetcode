eca55f3cad589823a1747ad262a95d4165a0d59e
Seoyoung Kim <70043001+syoungk7@users.noreply.github.com>
2023-08-15 11:18:31 -0400

    Time: 130 ms (75.71%), Space: 27 MB (64.35%) - LeetHub

--- 1506-find-root-of-n-ary-tree/1506-find-root-of-n-ary-tree.py
+++ 1506-find-root-of-n-ary-tree/1506-find-root-of-n-ary-tree.py
@@ -9,11 +9,13 @@ class Node:
 class Solution:
     def findRoot(self, tree: List['Node']) -> 'Node':
         if not tree: return None
-        tmp = []
-        
+        #tmp = []
+        tmp = set()
+
         for i in tree:
             for j in i.children:
-                if j.val not in tmp: tmp.append(j.val)
+                #if j.val not in tmp: tmp.append(j.val)
+                tmp.add(j.val)
                     
         for k in tree:
             if k.val not in tmp: return k
