b37135215b9bad1a65e377c28fc7afa044abfd61
Seoyoung Kim <70043001+syoungk7@users.noreply.github.com>
2023-08-25 18:09:15 -0400

    Time: 60 ms (91.95%), Space: 17.7 MB (13.92%) - LeetHub

--- 0001-two-sum/0001-two-sum.py
+++ 0001-two-sum/0001-two-sum.py
@@ -1,15 +1,27 @@
 class Solution:
     def twoSum(self, nums: List[int], target: int) -> List[int]:
         if len(nums) < 2: return []
+# solution using diff        
+        pair = {}
+        for idx in range(len(nums)):
+            diff = target - nums[idx]
+            
+            if diff in pair:
+                return pair[diff], idx
+            
+            pair[nums[idx]] = idx
+
+
         
-        for num in nums:
-            if target >= 0 and num <= target:
-                for idx, num2 in enumerate(nums):
-                    if num + num2 == target and nums.index(num) != idx:
-                        return [nums.index(num), idx]
+# solution using loop
+#         for num in nums:
+#             if target >= 0 and num <= target:
+#                 for idx, num2 in enumerate(nums):
+#                     if num + num2 == target and nums.index(num) != idx:
+#                         return [nums.index(num), idx]
                         
-            elif target < 0 and num >= target:
-                for idx, num2 in enumerate(nums):
-                    if num + num2 == target and nums.index(num) != idx:
-                        return [nums.index(num), idx]                
+#             elif target < 0 and num >= target:
+#                 for idx, num2 in enumerate(nums):
+#                     if num + num2 == target and nums.index(num) != idx:
+#                         return [nums.index(num), idx]                
             
