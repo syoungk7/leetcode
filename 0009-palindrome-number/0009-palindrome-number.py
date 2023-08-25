1322cbc76be9d83625b4a3b12448e09cf6721ea0
Seoyoung Kim <70043001+syoungk7@users.noreply.github.com>
2023-08-25 18:26:07 -0400

    Time: 38 ms (99.84%), Space: 16.2 MB (94.83%) - LeetHub

--- 0009-palindrome-number/0009-palindrome-number.py
+++ 0009-palindrome-number/0009-palindrome-number.py
@@ -1,13 +1,17 @@
 class Solution:
     def isPalindrome(self, x: int) -> bool:
         if x < 0: return False
-        if x == 0 : return True
-        
-        tmp_lst = []
-        for char in str(x): 
-            tmp_lst.append(char)
 
-        tmp = tmp_lst.copy()
-        tmp_lst.reverse()
-        print(tmp_lst, tmp)
-        if tmp_lst == tmp: return True
+        return str(x) == str(x)[::-1]
+        
+        
+#         if x == 0 : return True
+        
+#         tmp_lst = []
+#         for char in str(x): 
+#             tmp_lst.append(char)
+
+#         tmp = tmp_lst.copy()
+#         tmp_lst.reverse()
+#         print(tmp_lst, tmp)
+#         if tmp_lst == tmp: return True
