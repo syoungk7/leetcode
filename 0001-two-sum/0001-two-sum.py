2895bb47664ec2ae6bdf55c0ddea621a58d004f4
Seoyoung Kim <70043001+syoungk7@users.noreply.github.com>
2023-08-25 18:10:28 -0400

    Time: 2504 ms (29.49%), Space: 17.3 MB (59.48%) - LeetHub

--- 0001-two-sum/0001-two-sum.py
+++ 0001-two-sum/0001-two-sum.py
@@ -1,6 +1,14 @@
 class Solution:
     def twoSum(self, nums: List[int], target: int) -> List[int]:
         if len(nums) < 2: return []
+
+        
+
+        for i in range(len(nums)):
+            for j in range(i+1, len(nums)):
+                if nums[i] + nums[j] == target:
+                    return [i, j]
+                
 # solution using diff        
         pair = {}
         for idx in range(len(nums)):
@@ -11,8 +19,6 @@ class Solution:
             
             pair[nums[idx]] = idx
 
-
-        
 # solution using loop
 #         for num in nums:
 #             if target >= 0 and num <= target:
