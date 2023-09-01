e47db47197c1810c367b501e505b06b23461322f
Seoyoung Kim <70043001+syoungk7@users.noreply.github.com>
2023-09-01 19:49:39 -0400

    Time: 98 ms (77.75%), Space: 21.1 MB (66.75%) - LeetHub

--- 0347-top-k-frequent-elements/0347-top-k-frequent-elements.py
+++ 0347-top-k-frequent-elements/0347-top-k-frequent-elements.py
@@ -1,13 +1,15 @@
 class Solution:
     def topKFrequent(self, nums: List[int], k: int) -> List[int]:
         tmp_dic = dict()
-        out = []
+
+        # count nums
         for num in nums:
             if num not in tmp_dic:
                 tmp_dic[num] = 1
             else:
                 tmp_dic[num] += 1
 
+        # sort
         tmp_dic = sorted(tmp_dic.items(), key=lambda x: x[1], reverse=True)
         
         return [tmp_dic[i][0] for i in range(k)]
