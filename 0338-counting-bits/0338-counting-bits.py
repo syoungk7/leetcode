8c1abb40664f18f0fb2d65e1f8ee5f9f40ef50c6
Seoyoung Kim <70043001+syoungk7@users.noreply.github.com>
2023-09-02 00:16:19 -0400

    Time: 199 ms (8.20%), Space: 22.9 MB (94.90%) - LeetHub

---
+++ 0338-counting-bits/0338-counting-bits.py
@@ -0,0 +1,17 @@
+class Solution:
+    def countBits(self, n: int) -> List[int]:
+        out = [0]
+        
+        
+        if n == 0: return out
+        
+        for i in range(1, n+1):
+            tmp = 0
+            #print(list(str(format(i, 'b'))))
+            for j in list(str(format(i, 'b'))):
+                if int(j) == 1:
+                    tmp += 1
+            out.append(tmp)
+        return out
+            
+        
