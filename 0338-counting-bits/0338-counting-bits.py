d4a6e2097158017eb68aa9ef1379cf7921adb4f9
Seoyoung Kim <70043001+syoungk7@users.noreply.github.com>
2023-09-02 00:21:37 -0400

    Time: 83 ms (61.74%), Space: 23.1 MB (65.12%) - LeetHub

--- 0338-counting-bits/0338-counting-bits.py
+++ 0338-counting-bits/0338-counting-bits.py
@@ -4,12 +4,18 @@ class Solution:
         if n == 0: return out
         
         for i in range(1, n+1):
-            tmp = 0
-            #print(list(str(format(i, 'b'))))
-            for j in list(str(format(i, 'b'))):
-                if int(j) == 1:
-                    tmp += 1
+            tmp = format(i, 'b').count('1')
             out.append(tmp)
         return out
             
+#         dp = [0] * (n+1)
+#         for i in range(n+1):
+#             dp[i] = dp[i//2] + i % 2
         
+#         return dp
+
+        # a = []
+        # for i in range(n+1):
+        #     b = bin(i)[2:].count('1')
+        #     a.append(b)
+        # return a
