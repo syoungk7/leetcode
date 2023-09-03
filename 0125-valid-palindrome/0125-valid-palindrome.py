aac7d9a4388f13db94bf1d618bb8daf2c6b875e3
Seoyoung Kim <70043001+syoungk7@users.noreply.github.com>
2023-09-03 19:54:10 -0400

    Time: 57 ms (47.42%), Space: 17.5 MB (34.92%) - LeetHub

---
+++ 0125-valid-palindrome/0125-valid-palindrome.py
@@ -0,0 +1,7 @@
+class Solution:
+    def isPalindrome(self, s: str) -> bool:
+        if len(s) == 0: return True
+            
+        tmp = "".join(re.split("[^a-zA-Z0-9]*", s.lower()))
+        
+        return tmp == tmp[::-1]
