600b83625640f781a23f36b1febf650ebd3f3f39
Seoyoung Kim <70043001+syoungk7@users.noreply.github.com>
2023-09-03 21:37:16 -0400

    Time: 119 ms (74.22%), Space: 17.4 MB (15.02%) - LeetHub

---
+++ 0167-two-sum-ii-input-array-is-sorted/0167-two-sum-ii-input-array-is-sorted.py
@@ -0,0 +1,15 @@
+class Solution:
+    def twoSum(self, numbers: List[int], target: int) -> List[int]:
+        pair = defaultdict()
+        
+        for i in range(len(numbers)):
+            
+            if numbers[i] == target/2 and numbers[i] == numbers[i+1]:
+                return [i+1, i+2]
+            
+            elif numbers[i] != target/2 and numbers[i] not in pair:
+                pair[numbers[i]] = i+1
+            
+            if target - numbers[i] in pair:
+                return [pair[target - numbers[i]], i+1]
+            
