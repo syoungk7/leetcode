2206cac0d1bb9aab3027521f57d817528725c871
Seoyoung Kim <70043001+syoungk7@users.noreply.github.com>
2023-09-21 17:39:55 -0400

    Time: 1028 ms (86.91%), Space: 0B (100.00%) - LeetHub

--- 1873-calculate-special-bonus/1873-calculate-special-bonus.sql
+++ 1873-calculate-special-bonus/1873-calculate-special-bonus.sql
@@ -1,4 +1,4 @@
--- Write your MySQL query statement below
+/* Write your PL/SQL query statement below */
 
 Select employee_id, (Case When Mod(employee_id, 2) <> 0 And name Not Like 'M%' 
                      Then salary
