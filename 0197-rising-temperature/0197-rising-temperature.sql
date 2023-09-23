edc58c8336b21358ce4ac39409dea8a6f478a0cd
Seoyoung Kim <70043001+syoungk7@users.noreply.github.com>
2023-09-23 11:18:33 -0400

    Time: 658 ms (98.17%), Space: 0B (100.00%) - LeetHub

---
+++ 0197-rising-temperature/0197-rising-temperature.sql
@@ -0,0 +1,10 @@
+# Write your MySQL query statement below
+With tmp_weather (id, recordDate, temperature, prev_day, prev_tem)
+As (Select id, recordDate, temperature,
+    Lag(recordDate) over (Order by recordDate) prev_day,
+    Lag(temperature) over (Order by recordDate) prev_tem
+    From Weather
+    )
+Select id
+From tmp_weather
+Where temperature - prev_tem > 0 and TIMESTAMPDIFF(Day, prev_day, recordDate) = 1
