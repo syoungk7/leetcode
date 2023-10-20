1b922a6b3af2a77626fb23cea1acdf39cdd901cc
Seoyoung Kim <70043001+syoungk7@users.noreply.github.com>
2023-10-19 20:12:42 -0400

    Time: 98 ms (63.29%), Space: 21 MB (76.70%) - LeetHub

--- 0347-top-k-frequent-elements/0347-top-k-frequent-elements.py
+++ 0347-top-k-frequent-elements/0347-top-k-frequent-elements.py
@@ -1,26 +1,16 @@
 class Solution:
     def topKFrequent(self, nums: List[int], k: int) -> List[int]:
-        tmp_dic = dict()
-
-        # count nums
-        for num in nums:
-            if num not in tmp_dic:
-                tmp_dic[num] = 1
+        if len(nums) == 0: return []
+        
+        uniq = set(nums)
+        
+        from collections import defaultdict 
+        count_nums = defaultdict()
+        
+        for i in nums:
+            if i in count_nums:
+                count_nums[i] += 1
             else:
-                tmp_dic[num] += 1
+                count_nums[i] = 1
 
-        # sort
-        tmp_dic = sorted(tmp_dic.items(), key=lambda x: x[1], reverse=True)
-        
-        return [tmp_dic[i][0] for i in range(k)]
-
-# collections.Counter & heaoq    
-#         tmp = collections.Counter(A)
-#         nums = list(tmp.keys())
-#         res = [(-float("inf"), 0)]*k
-#         heapq.heapify(res)
-#         for num in nums:
-#             if tmp[num] > res[0][0]:
-#                 heapq.heapreplace(res, (tmp[num], num))
-#         return [i[1] for i in res]
-        
+        return [key for key, val in sorted(count_nums.items(), key=lambda item: item[1], reverse = True)][0:k]
