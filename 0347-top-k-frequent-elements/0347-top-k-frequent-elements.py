b69ce84371308f63940da37db8696868f071be1e
Seoyoung Kim <70043001+syoungk7@users.noreply.github.com>
2023-10-19 20:16:38 -0400

    Time: 81 ms (99.52%), Space: 21.2 MB (42.81%) - LeetHub

--- 0347-top-k-frequent-elements/0347-top-k-frequent-elements.py
+++ 0347-top-k-frequent-elements/0347-top-k-frequent-elements.py
@@ -1,7 +1,22 @@
 class Solution:
     def topKFrequent(self, nums: List[int], k: int) -> List[int]:
         if len(nums) == 0: return []
+        hash = {}
+        for num in nums:
+            hash[num] = 1 + hash.get(num, 0)
         
+#         ls = [(k,v) for k,v in hash.items()]
+#         ls.sort(key = lambda x: x[1])
+
+#         out = []
+
+#         for  i in range(k):
+#             out.append(ls[-1 - i][0])
+        
+        return [key for key, val in sorted(hash.items(), key=lambda item: item[1], reverse = True)][0:k]
+        
+        
+'''        
         uniq = set(nums)
         
         from collections import defaultdict 
@@ -13,4 +28,6 @@ class Solution:
             else:
                 count_nums[i] = 1
 
-        return [key for key, val in sorted(count_nums.items(), key=lambda item: item[1], reverse = True)][0:k]
+        return 
+'''
+
