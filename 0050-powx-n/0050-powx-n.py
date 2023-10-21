38beb7b9b805fb3c03447ef9a1a57f8ee1f875dc
Seoyoung Kim <70043001+syoungk7@users.noreply.github.com>
2023-10-20 22:53:14 -0400

    Time: 45 ms (14.76%), Space: 16 MB (97.50%) - LeetHub

--- 0050-powx-n/0050-powx-n.py
+++ 0050-powx-n/0050-powx-n.py
@@ -2,24 +2,25 @@ class Solution:
     def myPow(self, x: float, n: int) -> float:
         #return x ** n
 
-#         new_n = n if n > 0 else -n
-#         new_x = x if n > 0 else 1/x
-#         mul_x = 1
+        new_n = n if n > 0 else -n
+        new_x = x if n > 0 else 1/x
+        mul_x = 1
 
-#         while n != 0:
-#             if n & 1:
-#                 mul_x *= new_x
-#             new_x *= new_x
-#             n >>= 1
+        while new_n != 0:
+            if new_n % 2 == 1:
+                mul_x *= new_x
+                new_n -= 1
+            new_x *= new_x
+            new_n //= 2
 
-#         return mul_x
+        return mul_x
     
-        e = -n if n < 0 else n
-        res = 1.0
-        mult = 1 / x if n < 0 else x
-        while e != 0:
-            if e & 1:
-                res *= mult
-            mult *= mult
-            e >>= 1
-        return res
+        # e = -n if n < 0 else n
+        # res = 1.0
+        # mult = 1 / x if n < 0 else x
+        # while e != 0:
+        #     if e & 1:
+        #         res *= mult
+        #     mult *= mult
+        #     e >>= 1
+        # return res
