d5f07286be5bb0457988a1ad17264182dffb25ca
Seoyoung Kim <70043001+syoungk7@users.noreply.github.com>
2023-10-20 22:51:26 -0400

    Time: 42 ms (29.78%), Space: 16.1 MB (97.50%) - LeetHub

--- 0050-powx-n/0050-powx-n.py
+++ 0050-powx-n/0050-powx-n.py
@@ -1,6 +1,19 @@
 class Solution:
     def myPow(self, x: float, n: int) -> float:
         #return x ** n
+
+#         new_n = n if n > 0 else -n
+#         new_x = x if n > 0 else 1/x
+#         mul_x = 1
+
+#         while n != 0:
+#             if n & 1:
+#                 mul_x *= new_x
+#             new_x *= new_x
+#             n >>= 1
+
+#         return mul_x
+    
         e = -n if n < 0 else n
         res = 1.0
         mult = 1 / x if n < 0 else x
