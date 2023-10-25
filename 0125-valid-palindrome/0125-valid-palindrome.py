9c33bbcaadee3d3f3337b9cbe7a6e4dfa7a55fa0
Seoyoung Kim <70043001+syoungk7@users.noreply.github.com>
2023-10-24 21:05:18 -0400

    Time: 44 ms (84.17%), Space: 22.3 MB (7.05%) - LeetHub

--- 0125-valid-palindrome/0125-valid-palindrome.py
+++ 0125-valid-palindrome/0125-valid-palindrome.py
@@ -1,7 +1,11 @@
 class Solution:
     def isPalindrome(self, s: str) -> bool:
-        if len(s) == 0: return True
-
-        tmp = "".join(re.split("[^a-zA-Z0-9]*", s.lower()))
+        if len(s) == 0 or len(s) == 1: return True
         
-        return tmp == tmp[::-1]
+        s = [char.lower() for char in s if char.isalpha() == True or char.isdigit() == True]
+
+        A = ''.join(s)
+        s.reverse()
+
+        return A == ''.join(s)
+        
