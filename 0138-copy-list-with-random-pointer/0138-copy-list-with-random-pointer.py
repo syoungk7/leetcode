1d41e3ab137b057ce54659ee1d1b1cdf4507bb02
Seoyoung Kim <70043001+syoungk7@users.noreply.github.com>
2023-10-25 00:13:37 -0400

    Time: 35 ms (92.55%), Space: 17.7 MB (8.94%) - LeetHub

--- 0138-copy-list-with-random-pointer/0138-copy-list-with-random-pointer.py
+++ 0138-copy-list-with-random-pointer/0138-copy-list-with-random-pointer.py
@@ -13,14 +13,36 @@ class Solution:
         if head == None: return None
         
         out = {}
-        
         def new_head(node):
             if node == None: return None
             if node in out: return out[node]
+
             out[node] = Node(node.val)
             out[node].next = new_head(node.next)
             out[node].random = new_head(node.random)
-            
+
             return out[node]
 
         return new_head(head)
+    
+#         node = head
+#         node2 = head
+#         old, new, out = node, node.next, node.next
+#         while node2:
+#             new_node = Node(node.val, None, None)
+#             new_node.next = node.next
+#             node.next = new_node            
+#             node = new_node.next
+
+#             node2.next.random = node2.random.next if node2.random else None
+#             node2 = node2.next.next
+            
+#             if new:
+#                 old.next = node2
+#                 print(new, new.next)
+#                 new.next = node2.next if new.next else None
+#                 old = old.next
+#                 new = new.next
+        
+#         return out
+            
