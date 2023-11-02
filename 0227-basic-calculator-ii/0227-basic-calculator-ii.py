daa18665f169fc4ea914231149306f159c36e007
Seoyoung Kim <70043001+syoungk7@users.noreply.github.com>
2023-11-01 23:18:57 -0400

    Time: 917 ms (5.04%), Space: 18.4 MB (98.13%) - LeetHub

---
+++ 0227-basic-calculator-ii/0227-basic-calculator-ii.py
@@ -0,0 +1,38 @@
+class Solution:
+    def calculate(self, s: str) -> int:
+        oper = []
+        numb = []
+        pre_idx = 0
+
+        s = s.replace(" ", '')
+        calc = {'+': lambda i, j: i + j, '-': lambda i, j: i - j, '*': lambda i, j: i * j, '/': lambda i, j: i / j}
+
+        for idx, val in enumerate(s):
+            #if val != ' ':
+
+            if val in ['+', '-', '*', '/']:
+                numb.append(s[pre_idx: idx])
+                oper.append(val)
+                pre_idx = idx+1
+            else: pass
+
+            if idx == len(s)-1:
+                numb.append(s[pre_idx::])
+            print('0', oper, numb)
+            if len(oper) >= 2 and len(numb) == 3:
+                if oper[1] in ['+', '-'] or oper[0] == '/':
+                    tmp = calc[oper[0]](int(numb[0]), int(numb[1]))
+                    oper.pop(0)
+                    numb.pop(0)
+                    numb[0] = int(tmp)
+                    print('1', oper, numb)
+                else:
+                    tmp = calc[oper[1]](int(numb[1]), int(numb[2]))
+                    oper.pop(1)
+                    numb.pop(1)
+                    numb[1] = int(tmp)
+                    print('2', oper, numb)
+            #print(numb)
+        if len(oper) == 1:
+            return int(calc[oper[0]](int(numb[0]), int(numb[1])))
+        return int(numb[0])
