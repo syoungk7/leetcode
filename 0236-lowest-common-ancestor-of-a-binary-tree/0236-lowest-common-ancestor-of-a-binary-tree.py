a5de753d75a48d20cc7dab2b148619df368df6ed
Seoyoung Kim <70043001+syoungk7@users.noreply.github.com>
2023-11-06 17:52:31 -0500

    Time: 66 ms (58.32%), Space: 21 MB (91.83%) - LeetHub

--- 0236-lowest-common-ancestor-of-a-binary-tree/0236-lowest-common-ancestor-of-a-binary-tree.py
+++ 0236-lowest-common-ancestor-of-a-binary-tree/0236-lowest-common-ancestor-of-a-binary-tree.py
@@ -7,18 +7,41 @@
 
 class Solution:
     def lowestCommonAncestor(self, root: 'TreeNode', p: 'TreeNode', q: 'TreeNode') -> 'TreeNode':
-        if root == p or root == q:
-            return root
+#         if root == p or root == q:
+#             return root
         
-        left = right = None
-        if root.left:
-            left = self.lowestCommonAncestor(root.left, p, q)
-        if root.right:
-            right = self.lowestCommonAncestor(root.right, p, q)
+#         left = right = None
+#         if root.left:
+#             left = self.lowestCommonAncestor(root.left, p, q)
+#         if root.right:
+#             right = self.lowestCommonAncestor(root.right, p, q)
 
-        if left and right:
-            return root
-        elif left:
-            return left
-        else:
-            return right
+#         if left and right:
+#             return root
+#         elif left:
+#             return left
+#         else:
+#             return right
+
+
+        parent = {root: None}
+        queue = deque([root])
+
+        while not (p in parent and q in parent):
+            node = queue.popleft()
+            if node.left:
+                parent[node.left] = node
+                queue.append(node.left)
+            if node.right:
+                parent[node.right] = node
+                queue.append(node.right)
+
+        ancestors = {p}
+        while parent[p]:
+            ancestors.add(parent[p])
+            p = parent[p]
+
+        while q not in ancestors:
+            q = parent[q]
+        return q
+            
