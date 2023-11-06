4ff8cab5f637ed5d0a52628b73af7adf3302e947
Seoyoung Kim <70043001+syoungk7@users.noreply.github.com>
2023-11-06 18:49:55 -0500

    Time: 449 ms (70.53%), Space: 0B (100.00%) - LeetHub

--- 0595-big-countries/0595-big-countries.sql
+++ 0595-big-countries/0595-big-countries.sql
@@ -1,4 +1,4 @@
-/* Write your PL/SQL query statement below */
-SELECT name, population, area 
-FROM world
-WHERE area >= 3000000 OR population >= 25000000;
+# Write your MySQL query statement below
+select name, population, area
+from World
+where area >= 3000000 or population >= 25000000
