4a13d997edaa02ba7038de5097822b2172119529
Seoyoung Kim <70043001+syoungk7@users.noreply.github.com>
2023-11-06 19:25:57 -0500

    Time: 2158 ms (97.54%), Space: 0B (100.00%) - LeetHub

--- 1581-customer-who-visited-but-did-not-make-any-transactions/1581-customer-who-visited-but-did-not-make-any-transactions.sql
+++ 1581-customer-who-visited-but-did-not-make-any-transactions/1581-customer-who-visited-but-did-not-make-any-transactions.sql
@@ -3,4 +3,4 @@ select customer_id, count(customer_id) as count_no_trans
 from Visits
 where visit_id not in (select visit_id from Transactions)
 group by customer_id
-order by count_no_trans
+order by count_no_trans Desc
