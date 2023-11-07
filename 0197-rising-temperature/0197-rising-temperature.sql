4e4d6a72d520bfd398e5a6fadb95d17e7ed0ca21
Seoyoung Kim <70043001+syoungk7@users.noreply.github.com>
2023-11-07 18:53:56 -0500

    Time: 750 ms (79.61%), Space: 0B (100.00%) - LeetHub

--- 0197-rising-temperature/0197-rising-temperature.sql
+++ 0197-rising-temperature/0197-rising-temperature.sql
@@ -1,8 +1,22 @@
 # Write your MySQL query statement below
-# select w.id
-# from Weather w
-# Join Weather e on DATEDIFF(w.recordDate, e.recordDate) = 1 and w.temperature - e.temperature > 0
 
+## 예전
+# With tmp_weather (id, recordDate, temperature, prev_day, prev_tem)
+# As (Select id, recordDate, temperature,
+#     Lag(recordDate) over (Order by recordDate) prev_day,
+#     Lag(temperature) over (Order by recordDate) prev_tem
+#     From Weather
+#     )
+# Select id
+# From tmp_weather
+# Where temperature - prev_tem > 0 and TIMESTAMPDIFF(Day, prev_day, recordDate) = 1
+
+# join
 select w.id
-from Weather w, Weather e
-where DATEDIFF(w.recordDate, e.recordDate) = 1 and w.temperature - e.temperature > 0
+from Weather w
+Join Weather e on TIMESTAMPDIFF(Day, e.recordDate, w.recordDate) = 1 and w.temperature - e.temperature > 0
+
+# where
+# select w.id
+# from Weather w, Weather e
+# where TIMESTAMPDIFF(Day, w.recordDate, e.recordDate) = 1 and w.temperature - e.temperature > 0
