3880699d8f2f5630913a2e47dfae529541462b71
Seoyoung Kim <70043001+syoungk7@users.noreply.github.com>
2023-11-06 19:07:16 -0500

    Time: 2334 ms (47.78%), Space: 0B (100.00%) - LeetHub

--- 1378-replace-employee-id-with-the-unique-identifier/1378-replace-employee-id-with-the-unique-identifier.sql
+++ 1378-replace-employee-id-with-the-unique-identifier/1378-replace-employee-id-with-the-unique-identifier.sql
@@ -1,4 +1,4 @@
-/* Write your PL/SQL query statement below */
-SELECT EmployeeUNI.unique_id, Employees.name
-FROM Employees
-LEFT JOIN EmployeeUNI ON Employees.id = EmployeeUNI.id;
+# Write your MySQL query statement below
+select u.unique_id, e.name
+from Employees e
+left join EmployeeUNI u on e.id = u.id
