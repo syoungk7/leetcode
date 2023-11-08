cf779306415c920a4884166bbda7dff0fc19f11e
Seoyoung Kim <70043001+syoungk7@users.noreply.github.com>
2023-11-07 23:39:04 -0500

    Time: 1118 ms (93.76%), Space: 0B (100.00%) - LeetHub

--- 1934-confirmation-rate/1934-confirmation-rate.sql
+++ 1934-confirmation-rate/1934-confirmation-rate.sql
@@ -1,33 +1,32 @@
 # Write your MySQL query statement below
 
-# with noc as (
-# select user_id, count(action) as num_of_confirmed
-# from Confirmations
-# where action = 'confirmed'
-# group by user_id, action
-# )
-# # {"headers": ["user_id", "num_of_confirmed"], "values": [[7, 3], [2, 1]]}
+with noc as (
+select user_id, count(action) as num_of_confirmed
+from Confirmations
+where action = 'confirmed'
+group by user_id, action
+)
 
-# select s.user_id, ifnull(n.num_of_confirmed/count(c.user_id), 0) as confirmation_rate
-# from Signups s
-# left join Confirmations c on s.user_id = c.user_id
-# left join noc n on c.user_id = n.user_id
-# group by c.user_id
-# # {"headers": ["user_id", "num_of_action"], "values": [[3, 2], [7, 3], [2, 2], [6, 0]]}
-
-# select s.user_id, round(avg(if(c.action="confirmed", 1, 0)), 2) as confirmation_rate
-# from Signups s 
-# left join Confirmations c on s.user_id = c.user_id 
-# group by user_id
-
-
-
-Select s.user_id, ifnull(round((sum(c.action = 'confirmed')/count(c.action)), 2), 0) as confirmation_rate
-From Signups s
+select s.user_id, ifnull(round(n.num_of_confirmed/count(c.user_id), 2), 0) as confirmation_rate
+from Signups s
 left join Confirmations c on s.user_id = c.user_id
-group by s.user_id;
+left join noc n on c.user_id = n.user_id
+group by s.user_id
+# {"headers": ["user_id", "num_of_action"], "values": [[3, 2], [7, 3], [2, 2], [6, 0]]}
+
+
+
+# Select s.user_id, ifnull(round((sum(c.action = 'confirmed')/count(c.action)), 2), 0) as confirmation_rate
+# From Signups s
+# left join Confirmations c on s.user_id = c.user_id
+# group by s.user_id;
 
 # Select s.user_id, ifnull(round(sum(case when c.action='confirmed' then 1 else 0 end)/count(c.user_id), 2), 0) as confirmation_rate
 # From Signups s
 # left join Confirmations c on s.user_id = c.user_id
 # group by s.user_id;
+
+# select s.user_id, round(avg(if(c.action="confirmed", 1, 0)), 2) as confirmation_rate
+# from Signups s 
+# left join Confirmations c on s.user_id = c.user_id 
+# group by user_id
