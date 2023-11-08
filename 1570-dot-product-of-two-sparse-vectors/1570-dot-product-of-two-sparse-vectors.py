dbb86b7e6bd5aab27cc4819c9571e2f93d9951d6
Seoyoung Kim <70043001+syoungk7@users.noreply.github.com>
2023-11-07 22:05:50 -0500

    Time: 1367 ms (56.56%), Space: 17.2 MB (81.72%) - LeetHub

--- 1570-dot-product-of-two-sparse-vectors/1570-dot-product-of-two-sparse-vectors.py
+++ 1570-dot-product-of-two-sparse-vectors/1570-dot-product-of-two-sparse-vectors.py
@@ -3,10 +3,7 @@ class SparseVector:
         """
         :type nums: List[int]
         """
-        self.sv = {}
-        for idx, val in enumerate(nums):
-            if val != 0:
-                self.sv[idx] = val
+        self.sv = {idx:val for idx, val in enumerate(nums) if val != 0}
 
     # Return the dotProduct of two sparse vectors
     def dotProduct(self, vec):
@@ -15,12 +12,25 @@ class SparseVector:
         :rtype: int
         """
         output = 0
+        # if len(self.sv) == len(vec.sv):
+        #     for i in range(len(vec.sv)):
+        #         output += self.sv[i] * vec.sv[i]
+        # else: return None
+  
+        # if len(self.sv) > len(vec.sv):
+        #     for key in vec.sv.keys():
+        #         if key in self.sv:
+        #             output += self.sv[key] * vec.sv[key]
+        # else:
+        #      for key in self.sv.keys():
+        #         if key in vec.sv:
+        #             output += self.sv[key] * vec.sv[key]  
+
         shared_key = set(self.sv.keys()).intersection(set(vec.sv.keys()))
 
         for key in shared_key:
-            output += self.sv[key] * vec.sv[key]           
+            output += self.sv[key] * vec.sv[key]
         return output
-
 # Your SparseVector object will be instantiated and called as such:
 # v1 = SparseVector(nums1)
 # v2 = SparseVector(nums2)
