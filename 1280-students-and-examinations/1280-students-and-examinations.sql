8dbdf49ddae4cb4e88c15cf73a613c3b762b01dc
Seoyoung Kim <70043001+syoungk7@users.noreply.github.com>
2023-11-08 19:47:00 -0500

    Time: 1540 ms (80.25%), Space: 0B (100.00%) - LeetHub

--- 1280-students-and-examinations/1280-students-and-examinations.sql
+++ 1280-students-and-examinations/1280-students-and-examinations.sql
@@ -8,5 +8,5 @@ select s.student_id, s.student_name, s.subject_name, count(e.student_id) as atte
 from (select student_id, student_name, subject_name
       from Students, Subjects) s
 left join Examinations e on e.student_id = s.student_id and e.subject_name = s.subject_name
-group by s.student_id, s.student_name, s.subject_name
+group by s.student_id, s.subject_name
 order by s.student_id, s.subject_name
