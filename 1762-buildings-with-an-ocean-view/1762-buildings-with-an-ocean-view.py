0ce829b656ac6c701fdbb0506b2c756fef12232c
Seoyoung Kim <70043001+syoungk7@users.noreply.github.com>
2023-11-08 21:39:39 -0500

    Time: 615 ms (29.10%), Space: 33.9 MB (24.56%) - LeetHub

--- 1762-buildings-with-an-ocean-view/1762-buildings-with-an-ocean-view.py
+++ 1762-buildings-with-an-ocean-view/1762-buildings-with-an-ocean-view.py
@@ -1,13 +1,19 @@
 class Solution:
     def findBuildings(self, heights: List[int]) -> List[int]:
-        highest = 0
-        ocean_view = deque()
-        count = len(heights) -1
+#         highest = 0
+#         ocean_view = deque()
+#         count = len(heights) -1
         
-        while count != -1:
-            if highest < heights[count]:
-                ocean_view.appendleft(count)
-                highest = heights[count]
-            #highest = max(highest, heights[count])
-            count -= 1
-        return ocean_view
+#         while count != -1:
+#             if highest < heights[count]:
+#                 ocean_view.appendleft(count)
+#                 highest = heights[count]
+#             #highest = max(highest, heights[count])
+#             count -= 1
+#         return ocean_view
+        stack = []
+        for i, h in enumerate(heights):
+            while stack and heights[stack[-1]] <= h:
+                stack.pop()
+            stack.append(i)
+        return stack
