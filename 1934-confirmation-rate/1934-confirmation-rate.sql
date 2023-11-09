e1bd86d705bfa7a3c99ec02b055cbf206ac5386c
Seoyoung Kim <70043001+syoungk7@users.noreply.github.com>
2023-11-08 20:09:07 -0500

    Time: 1175 ms (79.52%), Space: 0B (100.00%) - LeetHub

--- 1934-confirmation-rate/1934-confirmation-rate.sql
+++ 1934-confirmation-rate/1934-confirmation-rate.sql
@@ -14,15 +14,18 @@ left join noc n on c.user_id = n.user_id
 group by s.user_id
 # {"headers": ["user_id", "num_of_action"], "values": [[3, 2], [7, 3], [2, 2], [6, 0]]}
 
+
 # Select s.user_id, ifnull(round((sum(c.action = 'confirmed')/count(c.action)), 2), 0) as confirmation_rate
 # From Signups s
 # left join Confirmations c on s.user_id = c.user_id
 # group by s.user_id;
 
+
 # Select s.user_id, ifnull(round(sum(case when c.action='confirmed' then 1 else 0 end)/count(c.user_id), 2), 0) as confirmation_rate
 # From Signups s
 # left join Confirmations c on s.user_id = c.user_id
 # group by s.user_id;
+
 
 # select s.user_id, round(avg(if(c.action="confirmed", 1, 0)), 2) as confirmation_rate
 # from Signups s 
