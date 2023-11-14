0595d07911a1049c6e508c57aa5dc37d5a56bf5c
Seoyoung Kim <70043001+syoungk7@users.noreply.github.com>
2023-11-13 22:29:34 -0500

    Time: 25 ms (99.42%), Space: 16.2 MB (66.67%) - LeetHub

--- 0921-minimum-add-to-make-parentheses-valid/0921-minimum-add-to-make-parentheses-valid.py
+++ 0921-minimum-add-to-make-parentheses-valid/0921-minimum-add-to-make-parentheses-valid.py
@@ -1,28 +1,27 @@
 class Solution:
     def minAddToMakeValid(self, s: str) -> int:
-
-        stack = []
-        count = 0
-        for par in s:
-            if par == '(':
-                stack.append(par)
-            elif stack and par == ')':
-                stack.pop()
-            elif not stack and par == ')':
-                count += 1
-
-        return len(stack) + count
-
+#         stack = []
 #         count = 0
-#         curr = 0
 #         for par in s:
 #             if par == '(':
-#                 count += 1
-#                 curr += 1
-#             elif curr != 0 and par == ')':
-#                 count -= 1
-#                 curr -= 1
-#             elif curr == 0 and par == ')':
+#                 stack.append(par)
+#             elif stack and par == ')':
+#                 stack.pop()
+#             elif not stack and par == ')':
 #                 count += 1
 
-#         return abs(count)
+#         return len(stack) + count
+
+        count = 0
+        curr = 0
+        for par in s:
+            if par == '(':
+                count += 1
+                curr += 1
+            elif curr != 0 and par == ')':
+                count -= 1
+                curr -= 1
+            elif curr == 0 and par == ')':
+                count += 1
+
+        return abs(count)
