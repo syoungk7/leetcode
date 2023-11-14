f197ec5f61c7fc80441d85707a275d9da7a51a3d
Seoyoung Kim <70043001+syoungk7@users.noreply.github.com>
2023-11-14 02:51:23 -0500

    Time: 66 ms (49.60%), Space: 20.6 MB (69.93%) - LeetHub

--- 1650-lowest-common-ancestor-of-a-binary-tree-iii/1650-lowest-common-ancestor-of-a-binary-tree-iii.py
+++ 1650-lowest-common-ancestor-of-a-binary-tree-iii/1650-lowest-common-ancestor-of-a-binary-tree-iii.py
@@ -10,21 +10,21 @@ class Node:
 
 class Solution:
     def lowestCommonAncestor(self, p: 'Node', q: 'Node') -> 'Node':
-        tmp = set()
+#         tmp = set()
         
-        def recur_ances(node):
-            if node is None or node in tmp:
-                return node
-            else:
-                tmp.add(node)
-                node = node.parent
-                return recur_ances(node)
+#         def recur_ances(node):
+#             if node is None or node in tmp:
+#                 return node
+#             else:
+#                 tmp.add(node)
+#                 node = node.parent
+#                 return recur_ances(node)
 
-        return recur_ances(p) or recur_ances(q)
+#         return recur_ances(p) or recur_ances(q)
 
-#         p1, p2 = p, q
-#         while p1 != p2:
-#             p1 = p1.parent if p1.parent else q
-#             p2 = p2.parent if p2.parent else p
+        left, right = p, q
+        while left != right:
+            left = left.parent if left.parent else q
+            right = right.parent if right.parent else p
             
-#         return p1
+        return left or right
