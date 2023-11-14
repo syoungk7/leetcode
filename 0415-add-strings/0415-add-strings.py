fd81dff816363fabf7970d31f1b56d4d72dd22f3
Seoyoung Kim <70043001+syoungk7@users.noreply.github.com>
2023-11-14 03:32:15 -0500

    Time: 53 ms (22.56%), Space: 16.4 MB (47.49%) - LeetHub

--- 0415-add-strings/0415-add-strings.py
+++ 0415-add-strings/0415-add-strings.py
@@ -3,19 +3,21 @@ class Solution:
 
         add_Char = 0
         lst_Char = []
-
+        ord_0 = ord('0')
+        
         while num1 != '' or num2 != '' or add_Char < 0:
             if len(num1) > 0:
-                add_Char += ord(num1[-1]) - ord('0')
+                add_Char += ord(num1[-1]) - ord_0
                 num1 = num1[:-1]
             
             if len(num2) > 0:
-                add_Char += ord(num2[-1]) - ord('0')
+                add_Char += ord(num2[-1]) - ord_0
                 num2 = num2[:-1]           
             
-            lst_Char.append(chr(add_Char % 10 + ord('0')))
+            lst_Char.append(chr(add_Char % 10 + ord_0))
             add_Char //= 10
+
             if num1 == '' and num2 == '' and add_Char != 0:
-                lst_Char.append(chr(add_Char + ord('0')))
+                lst_Char.append(chr(add_Char + ord_0))
 
         return "".join(lst_Char)[::-1]
