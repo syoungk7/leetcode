f1b9272d716aa9ec206bd1befcd9d1ce810a83f5
Seoyoung Kim <70043001+syoungk7@users.noreply.github.com>
2023-11-13 21:58:37 -0500

    Time: 33 ms (91.49%), Space: 16.2 MB (44.01%) - LeetHub

--- 0199-binary-tree-right-side-view/0199-binary-tree-right-side-view.py
+++ 0199-binary-tree-right-side-view/0199-binary-tree-right-side-view.py
@@ -7,15 +7,27 @@
 class Solution:
     def rightSideView(self, root: Optional[TreeNode]) -> List[int]:
         if root is None: return []
-
-        self.out = []
-        level = 0
-        def recur_right(root, level):
-            if level == len(self.out):
-                self.out.append(root.val)
+#        self.out = []
+#         level = 0
+#         def recur_right(root, level):
+#             if level == len(self.out):
+#                 self.out.append(root.val)
             
-            for i in [root.right, root.left]:
-                if i: recur_right(i, level+1)
+#             for i in [root.right, root.left]:
+#                 if i: recur_right(i, level+1)
 
-        recur_right(root, level)
-        return self.out
+#         recur_right(root, level)
+#         return self.out
+
+        out = []
+        tmp = [root]
+        while tmp:
+            out.append(tmp[-1].val)
+            children = []
+            for next_node in tmp:
+                if next_node.left:
+                    children.append(next_node.left)
+                if next_node.right:
+                    children.append(next_node.right)
+            tmp = children
+        return out
