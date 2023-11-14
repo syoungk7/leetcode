e847cf94633a303ddc549af8bbf2594f7a644f6f
Seoyoung Kim <70043001+syoungk7@users.noreply.github.com>
2023-11-13 22:29:12 -0500

    Time: 46 ms (9.12%), Space: 16.2 MB (66.67%) - LeetHub

--- 0921-minimum-add-to-make-parentheses-valid/0921-minimum-add-to-make-parentheses-valid.py
+++ 0921-minimum-add-to-make-parentheses-valid/0921-minimum-add-to-make-parentheses-valid.py
@@ -1,15 +1,28 @@
 class Solution:
     def minAddToMakeValid(self, s: str) -> int:
+
+        stack = []
         count = 0
-        curr = 0
         for par in s:
             if par == '(':
-                count += 1
-                curr += 1
-            elif curr != 0 and par == ')':
-                count -= 1
-                curr -= 1
-            elif curr == 0 and par == ')':
+                stack.append(par)
+            elif stack and par == ')':
+                stack.pop()
+            elif not stack and par == ')':
                 count += 1
 
-        return abs(count)
+        return len(stack) + count
+
+#         count = 0
+#         curr = 0
+#         for par in s:
+#             if par == '(':
+#                 count += 1
+#                 curr += 1
+#             elif curr != 0 and par == ')':
+#                 count -= 1
+#                 curr -= 1
+#             elif curr == 0 and par == ')':
+#                 count += 1
+
+#         return abs(count)
