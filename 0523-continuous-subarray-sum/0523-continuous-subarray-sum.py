2dc920f166caff1458a76f830ae7dc861f939226
Seoyoung Kim <70043001+syoungk7@users.noreply.github.com>
2023-11-14 00:20:47 -0500

    Time: 949 ms (35.43%), Space: 36.5 MB (6.28%) - LeetHub

--- 0523-continuous-subarray-sum/0523-continuous-subarray-sum.py
+++ 0523-continuous-subarray-sum/0523-continuous-subarray-sum.py
@@ -1,17 +1,32 @@
 class Solution:
     def checkSubarraySum(self, nums: List[int], k: int) -> bool:
         ## Time: O(N), Space: O(K)
+        if len(nums) < 2: return False
 
         hash_map = {0: 0}
         summa = 0
         
-        # for idx, val in enumerate(nums):
-        for idx in range(len(nums)):
-            summa += nums[idx]
-            if summa % k not in hash_map:
-                hash_map[summa % k] = idx + 1
-
-            elif summa % k in hash_map and hash_map[summa % k] < idx:
+        for idx, val in enumerate(nums):
+            summa += val
+            if summa % k in hash_map:
+                if hash_map[summa % k] < idx:
+                    print(hash_map, summa % k, idx)
                     return True
-
+            else:
+                hash_map[summa % k] = idx + 1
+    
         return False
+
+
+#         s = nums[0]
+#         lagging_s = 0
+#         subsums = {0: 1}
+#         for i in range(1, len(nums)):
+#             s += nums[i]
+#             s = s % k
+#             if s in subsums:
+#                 return True
+#             lagging_s = (lagging_s + nums[i - 1]) % k
+#             subsums[lagging_s] = 1
+#         return False
+    
