d027e47fcb90402618649cfa57a4e34e64b83fe9
Seoyoung Kim <70043001+syoungk7@users.noreply.github.com>
2023-11-14 00:45:40 -0500

    Time: 958 ms (24.61%), Space: 36 MB (49.46%) - LeetHub

--- 0523-continuous-subarray-sum/0523-continuous-subarray-sum.py
+++ 0523-continuous-subarray-sum/0523-continuous-subarray-sum.py
@@ -6,14 +6,14 @@ class Solution:
         hash_map = {0: 0}
         summa = 0
         
-        for idx, val in enumerate(nums):
-            summa += val
+        # for idx, val in enumerate(nums):
+        for idx in range(len(nums)):
+            summa += nums[idx]
             if summa % k in hash_map:
-                if (idx + 1) - hash_map[summa % k] >= 2 :
-                    print(hash_map, summa % k, idx)
+                if (idx+1) - hash_map[summa % k] >= 2:
                     return True
             else:
-                hash_map[summa % k] = idx + 1
+                hash_map[summa % k] = idx+1
     
         return False
 
