641afe8420e921f4dfe5943b515331cf89059e04
Seoyoung Kim <70043001+syoungk7@users.noreply.github.com>
2023-11-14 19:59:04 -0500

    Time: 705 ms (76.59%), Space: 0B (100.00%) - LeetHub

--- 0619-biggest-single-number/0619-biggest-single-number.sql
+++ 0619-biggest-single-number/0619-biggest-single-number.sql
@@ -1,8 +1,17 @@
 # Write your MySQL query statement below
+
 SELECT MAX(s.num) AS num
 FROM (SELECT num
       FROM MyNumbers
       GROUP BY num
       HAVING COUNT(num) = 1) s
-#ORDER BY num DESC
-#LIMIT 1
+
+# SELECT IF(s.num is null, null, s.num) AS num
+# SELECT IFNULL(s.num, null) AS num
+# SELECT num
+# FROM (SELECT NULLIF(num, NULL) AS num
+#       FROM MyNumbers
+#       GROUP BY num
+#       HAVING COUNT(num) = 1
+#       ORDER BY num DESC
+#       LIMIT 1) s
