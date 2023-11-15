987619b951edc2cd80af8155d548aa14c46adf76
Seoyoung Kim <70043001+syoungk7@users.noreply.github.com>
2023-11-14 21:40:44 -0500

    Time: 23 ms (99.73%), Space: 16.3 MB (29.78%) - LeetHub

--- 0921-minimum-add-to-make-parentheses-valid/0921-minimum-add-to-make-parentheses-valid.py
+++ 0921-minimum-add-to-make-parentheses-valid/0921-minimum-add-to-make-parentheses-valid.py
@@ -12,16 +12,29 @@ class Solution:
 
 #         return len(stack) + count
 
-        count = 0
-        curr = 0
-        for par in s:
-            if par == '(':
-                count += 1
-                curr += 1
-            elif curr != 0 and par == ')':
-                count -= 1
-                curr -= 1
-            elif curr == 0 and par == ')':
-                count += 1
-
-        return abs(count)
+        # count = 0
+        # curr = 0
+        # for par in s:
+        #     if par == '(':
+        #         count += 1
+        #         curr += 1
+        #     elif curr != 0 and par == ')':
+        #         count -= 1
+        #         curr -= 1
+        #     elif curr == 0 and par == ')':
+        #         count += 1
+        # return abs(count)
+        
+        i = res = 0
+        while i < len(s):
+            l = r = 0
+            j = i
+            while j < len(s) and l >= r:
+                if s[j] == '(':
+                    l += 1
+                elif s[j] == ')':
+                    r += 1
+                j += 1
+            i = j
+            res += abs(l - r)
+        return res
