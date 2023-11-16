e1305bf69ac31b70400a88ac71919eb12d793b99
Seoyoung Kim <70043001+syoungk7@users.noreply.github.com>
2023-11-16 18:40:46 -0500

    Time: 214 ms (56.41%), Space: 18 MB (6.39%) - LeetHub

--- 0704-binary-search/0704-binary-search.py
+++ 0704-binary-search/0704-binary-search.py
@@ -1,4 +1,16 @@
 class Solution:
     def search(self, nums: List[int], target: int) -> int:
-        if target in nums: return nums.index(target)
-        else: return -1
+        
+        # # TC: O(N), SC(1)
+        # if target in nums: 
+        #     return nums.index(target)
+        # else: 
+        #     return -1
+        
+         # TC: O(worst -> N), SC(1)
+        if target in nums[0:int(len(nums)/2)]: 
+            return nums.index(target)
+        elif target in nums[int(len(nums)/2)::]:
+            return nums.index(target)
+        else: 
+            return -1
