59f4bb3a3c13977ffa883ef968fe5c30f609c377
Seoyoung Kim <70043001+syoungk7@users.noreply.github.com>
2023-11-16 18:55:56 -0500

    Time: 223 ms (15.98%), Space: 17.8 MB (34.63%) - LeetHub

--- 0704-binary-search/0704-binary-search.py
+++ 0704-binary-search/0704-binary-search.py
@@ -1,16 +1,31 @@
 class Solution:
     def search(self, nums: List[int], target: int) -> int:
         
-        # # TC: O(N), SC(1)
+        ## TC: O(N), SC(1)
         # if target in nums: 
         #     return nums.index(target)
         # else: 
         #     return -1
         
-         # TC: O(worst -> N), SC(1)
-        if target in nums[0:int(len(nums)/2)]: 
-            return nums.index(target)
-        elif target in nums[int(len(nums)/2)::]:
-            return nums.index(target)
-        else: 
-            return -1
+        ## TC: O(worst -> N), SC(1)
+        # if target in nums[0:int(len(nums)/2)]: 
+        #     return nums.index(target)
+        # elif target in nums[int(len(nums)/2)::]:
+        #     return nums.index(target)
+        # else: 
+        #     return -1
+        
+        ## nums: sorted in ascending order
+        
+        right, left = 0, len(nums)-1 ## index
+        while right <= left:
+            middle = int((right + left)/2)
+
+            if nums[middle] < target:
+                right += 1
+            elif nums[middle] > target:
+                left -= 1
+            else:
+                return middle
+
+        return -1
