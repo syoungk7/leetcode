d25d83490c493777996ff9c1d4f83fc9158a3a58
Seoyoung Kim <70043001+syoungk7@users.noreply.github.com>
2023-11-16 21:35:21 -0500

    Time: 56 ms (45.91%), Space: 17.3 MB (21.09%) - LeetHub

--- 0035-search-insert-position/0035-search-insert-position.py
+++ 0035-search-insert-position/0035-search-insert-position.py
@@ -3,20 +3,23 @@ class Solution:
         ## nums: sorted in ascending order
         ## TC: O(logN), SC(1)
         if target <= nums[0]: return 0
-        if target == nums[-1]: return len(nums) -1
+        if target == nums[-1]: return len(nums)-1
         if target > nums[-1]: return len(nums)
+        if len(nums) < 3:
+            if target in nums: return nums.index(target)
+            else: return 1
         
-        start, end = 0, len(nums) - 1 ## index
+        right, left = 0, len(nums) - 1 ## index
 
-        while start <= end:
-            middle = int((start + end)/2)
+        while right <= left:
+            middle = int((right + left)/2)
 
             if nums[middle] < target:
-                start += 1
+                right = middle + 1
             elif nums[middle] > target:
-                end -= 1
+                left = middle - 1
             else:
                 if nums[middle] == target:
                     return middle
-
-        return start
+            print(right, left)
+        return right
