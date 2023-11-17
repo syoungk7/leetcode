9a0453f936906e95c001583db426c4c7efdc43de
Seoyoung Kim <70043001+syoungk7@users.noreply.github.com>
2023-11-17 14:30:39 -0500

    Time: 1584 ms (5.00%), Space: 0B (100.00%) - LeetHub

--- 0176-second-highest-salary/0176-second-highest-salary.sql
+++ 0176-second-highest-salary/0176-second-highest-salary.sql
@@ -1,10 +1,18 @@
 # Write your MySQL query statement below
-WITH selected AS (SELECT '2' as ranks, salary
-                FROM Employee
-                GROUP BY salary
-                ORDER BY salary DESC
-                LIMIT 1 OFFSET 1)
+# WITH selected AS (SELECT '2' as ranks, salary
+#                 FROM Employee
+#                 GROUP BY salary
+#                 ORDER BY salary DESC
+#                 LIMIT 1 OFFSET 1)
 
-SELECT e.salary AS SecondHighestSalary
-FROM selected e
-RIGHT JOIN (SELECT '2' as ranks) s ON s.ranks = e.ranks
+# SELECT e.salary AS SecondHighestSalary
+# FROM selected e
+# RIGHT JOIN (SELECT '2' as ranks) s ON s.ranks = e.ranks
+
+SELECT
+    IFNULL(
+      (SELECT DISTINCT Salary
+       FROM Employee
+       ORDER BY Salary DESC
+        LIMIT 1 OFFSET 1),
+    NULL) AS SecondHighestSalary
