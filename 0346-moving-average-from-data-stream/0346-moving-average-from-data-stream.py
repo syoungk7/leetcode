623098ec32fbf8281cc4aa8c14ac6f286d16ab79
Seoyoung Kim <idellio007@gmail.com>
2023-11-28 18:50:30 -0500

    Time: 86 ms (31.95%), Space: 19.5 MB (84.67%) - LeetHub

---
+++ 0346-moving-average-from-data-stream/0346-moving-average-from-data-stream.py
@@ -0,0 +1,21 @@
+class MovingAverage:
+
+    def __init__(self, size: int):
+        self.size = size
+        self.int_lst = []
+
+    def next(self, val: int) -> float:
+        self.int_lst.append(val)
+
+        if len(self.int_lst) < self.size:
+            return sum(self.int_lst) / len(self.int_lst)
+        elif len(self.int_lst) == self.size:
+            return sum(self.int_lst) / self.size
+        else:
+            self.int_lst.pop(0)
+            return sum(self.int_lst) / self.size
+
+
+# Your MovingAverage object will be instantiated and called as such:
+# obj = MovingAverage(size)
+# param_1 = obj.next(val)
