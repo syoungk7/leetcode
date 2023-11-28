3333b9b0fe2c7f74ed9f4bbab17e830f1cc5919a
Seoyoung Kim <idellio007@gmail.com>
2023-11-28 18:58:10 -0500

    Time: 96 ms (17.67%), Space: 19.5 MB (84.67%) - LeetHub

--- 0346-moving-average-from-data-stream/0346-moving-average-from-data-stream.py
+++ 0346-moving-average-from-data-stream/0346-moving-average-from-data-stream.py
@@ -6,9 +6,9 @@ class MovingAverage:
 
     def next(self, val: int) -> float:
         self.int_lst.append(val)
-
-        if len(self.int_lst) <= self.size:
-            return sum(self.int_lst) / len(self.int_lst)
+        len_lst = len(self.int_lst)
+        if len_lst <= self.size:
+            return sum(self.int_lst) / len_lst
         else:
             self.int_lst.pop(0)
             return sum(self.int_lst) / self.size
