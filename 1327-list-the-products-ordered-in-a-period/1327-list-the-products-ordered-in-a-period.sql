767cd68c5d9a87917bc76a1edcac3f51a3d2f336
Seoyoung Kim <idellio007@gmail.com>
2023-11-28 19:31:18 -0500

    Time: 1391 ms (50.53%), Space: 0B (100.00%) - LeetHub

--- 1327-list-the-products-ordered-in-a-period/1327-list-the-products-ordered-in-a-period.sql
+++ 1327-list-the-products-ordered-in-a-period/1327-list-the-products-ordered-in-a-period.sql
@@ -2,7 +2,8 @@
 SELECT p.product_name, s.unit
 FROM Products p, (SELECT product_id, SUM(unit) AS unit
                   FROM Orders
-                  where left(order_date, 7) = "2020-02"
+                  WHERE order_date BETWEEN '2020-02-01' and '2020-02-29'
+                  # where left(order_date, 7) = "2020-02"
                   GROUP BY product_id
                   HAVING SUM(unit) >= 100
                  ) s
