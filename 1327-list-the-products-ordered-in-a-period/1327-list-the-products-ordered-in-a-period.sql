97206705286454f0951c8d1420126f0068cdc339
Seoyoung Kim <idellio007@gmail.com>
2023-11-28 19:08:28 -0500

    Time: 1256 ms (83.66%), Space: 0B (100.00%) - LeetHub

--- 1327-list-the-products-ordered-in-a-period/1327-list-the-products-ordered-in-a-period.sql
+++ 1327-list-the-products-ordered-in-a-period/1327-list-the-products-ordered-in-a-period.sql
@@ -1,15 +1,15 @@
 # Write your MySQL query statement below
-# SELECT p.product_name, s.unit
-# FROM Products p, (SELECT product_id, SUM(unit) AS unit
-#                   FROM Orders
-#                   WHERE order_date BETWEEN '2020-02-01' and '2020-02-29'
-#                   GROUP BY product_id
-#                   HAVING SUM(unit) >= 100
-#                  ) s
-# WHERE p.product_id = s.product_id
+SELECT p.product_name, s.unit
+FROM Products p, (SELECT product_id, SUM(unit) AS unit
+                  FROM Orders
+                  where left(order_date, 7) = "2020-02"
+                  GROUP BY product_id
+                  HAVING SUM(unit) >= 100
+                 ) s
+WHERE p.product_id = s.product_id
 
-SELECT p.product_name AS product_name, sum(o.unit) AS unit FROM Products p
-JOIN Orders o USING (product_id)
-WHERE YEAR(o.order_date)='2020' AND MONTH(o.order_date)='02'
-GROUP BY p.product_id
-HAVING SUM(o.unit)>=100
+# SELECT p.product_name AS product_name, sum(o.unit) AS unit FROM Products p
+# JOIN Orders o USING (product_id)
+# WHERE YEAR(o.order_date)='2020' AND MONTH(o.order_date)='02'
+# GROUP BY p.product_id
+# HAVING SUM(o.unit)>=100
