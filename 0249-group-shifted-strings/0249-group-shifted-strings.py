6c423ed1b0d67480901797674d17265ec310e290
Seoyoung Kim <idellio007@gmail.com>
2023-11-29 21:49:40 -0500

    Time: 43 ms (60.19%), Space: 16.4 MB (14.21%) - LeetHub

---
+++ 0249-group-shifted-strings/0249-group-shifted-strings.py
@@ -0,0 +1,15 @@
+class Solution:
+    def groupStrings(self, strings: List[str]) -> List[List[str]]:
+        group = defaultdict()
+        convert = []
+
+        for i in strings:
+            for j in i:             
+                    convert.append((ord(j) - ord(i[0])) % 26)
+            if tuple(convert) in group:
+                group[tuple(convert)].append(i)
+            else:
+                group[tuple(convert)] = [i]
+
+            convert = []
+        return group.values()
