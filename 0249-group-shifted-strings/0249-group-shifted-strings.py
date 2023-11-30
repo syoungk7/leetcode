5f28fbbf968e1535f3d54ca7f85b17761864dcdd
Seoyoung Kim <idellio007@gmail.com>
2023-11-29 21:52:56 -0500

    Time: 38 ms (87.98%), Space: 16.5 MB (14.21%) - LeetHub

--- 0249-group-shifted-strings/0249-group-shifted-strings.py
+++ 0249-group-shifted-strings/0249-group-shifted-strings.py
@@ -6,10 +6,11 @@ class Solution:
         for i in strings:
             for j in i:             
                     convert.append((ord(j) - ord(i[0])) % 26)
+
             if tuple(convert) in group:
                 group[tuple(convert)].append(i)
             else:
                 group[tuple(convert)] = [i]
-
             convert = []
+    
         return group.values()
