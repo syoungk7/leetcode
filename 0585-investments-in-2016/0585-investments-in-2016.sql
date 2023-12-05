3807a77d6096993ee86e61f0f73f02aca8ed5f49
Seoyoung Kim <idellio007@gmail.com>
2023-12-04 19:19:08 -0500

    Time: 1019 ms (36.96%), Space: 0B (100.00%) - LeetHub

---
+++ 0585-investments-in-2016/0585-investments-in-2016.sql
@@ -0,0 +1,28 @@
+# Write your MySQL query statement below
+
+# SELECT tiv_2015
+# FROM Insurance
+# GROUP BY tiv_2015
+# HAVING COUNT(tiv_2015) != 1
+
+# SELECT lat, lon
+# FROM Insurance
+# GROUP BY lat, lon
+# HAVING COUNT(tiv_2015) = 1
+
+
+SELECT ROUND(SUM(tiv_2016), 2) AS tiv_2016
+FROM Insurance
+WHERE tiv_2015 in (
+    SELECT tiv_2015
+    FROM Insurance
+    GROUP BY tiv_2015
+    HAVING COUNT(tiv_2015) != 1
+    ) 
+    AND
+    (lat, lon) in (
+    SELECT lat, lon
+    FROM Insurance
+    GROUP BY lat, lon
+    HAVING COUNT(tiv_2015) = 1
+    )
