049a5040ab2bdeceaf0a3026157922665b8c68b9
Seoyoung Kim <idellio007@gmail.com>
2023-12-05 18:58:12 -0500

    Time: 88 ms (45.28%), Space: 16.5 MB (8.76%) - LeetHub

--- 0766-toeplitz-matrix/0766-toeplitz-matrix.py
+++ 0766-toeplitz-matrix/0766-toeplitz-matrix.py
@@ -1,14 +1,25 @@
 class Solution:
     def isToeplitzMatrix(self, matrix: List[List[int]]) -> bool:
-        de = deque(matrix[0])
-        print(deque)
-        
         for i in range(len(matrix)-1):
-            de.pop()
-    
-            if list(de) == matrix[i+1][1::]:
-                de.extendleft([matrix[i+1][0]])
-
-            else: return False
+            if matrix[i][0:len(matrix[i])-1] != matrix[i+1][1::]:
+                return False
+        else:
+            return True
             
-        return True
+            
+            
+#         de = deque(matrix[0])
+        
+#         for i in range(len(matrix)-1):
+#             de.pop()
+    
+#             if list(de) == matrix[i+1][1::]:
+#                 de.extendleft([matrix[i+1][0]])
+#             else: 
+#                 return False
+            
+#         return True
+
+
+        
+        
