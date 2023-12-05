41fa883be2ffa07ff8ad91c5caaebd77aa68d8ee
Seoyoung Kim <idellio007@gmail.com>
2023-12-05 18:59:59 -0500

    Time: 90 ms (32.77%), Space: 16.3 MB (36.55%) - LeetHub

--- 0766-toeplitz-matrix/0766-toeplitz-matrix.py
+++ 0766-toeplitz-matrix/0766-toeplitz-matrix.py
@@ -1,7 +1,6 @@
 class Solution:
     def isToeplitzMatrix(self, matrix: List[List[int]]) -> bool:
         
-        # 
         for i in range(len(matrix)-1):
             if matrix[i][0:len(matrix[i])-1] != matrix[i+1][1::]:
                 return False
