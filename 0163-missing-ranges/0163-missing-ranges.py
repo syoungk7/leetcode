a46ca369bd605188d028a28ef7d72b71d0b90a74
Seoyoung Kim <idellio007@gmail.com>
2023-12-06 21:19:47 -0500

    Time: 30 ms (94.82%), Space: 16.4 MB (24.20%) - LeetHub

--- 0163-missing-ranges/0163-missing-ranges.py
+++ 0163-missing-ranges/0163-missing-ranges.py
@@ -1,15 +1,37 @@
 class Solution:
     def findMissingRanges(self, nums: List[int], lower: int, upper: int) -> List[List[int]]:
+#         new = []
+#         if len(nums) == 0:
+#             new.append([lower, upper])
+#             return new
+
+#         new.append([lower, nums[0]-1]) if nums[0] > lower else None
+    
+#         for leng in range(0, len(nums)-1):
+#             new.append([nums[leng]+1, nums[leng+1]-1]) if (nums[leng+1] - nums[leng]) > 1 else None
+
+#         new.append([(nums[-1])+1, upper]) if nums[-1] < upper else None
+
+#         return new
+    
+    
         new = []
+        
         if len(nums) == 0:
             new.append([lower, upper])
             return new
 
-        new.append([lower, nums[0]-1]) if nums[0] > lower else None
-    
-        for leng in range(0, len(nums)-1):
-            new.append([nums[leng]+1, nums[leng+1]-1]) if (nums[leng+1] - nums[leng]) > 1 else None
+        elif nums[0] > lower:
+            new.append([lower, nums[0]-1])
 
-        new.append([(nums[-1])+1, upper]) if nums[-1] < upper else None
+        for leng in range(0, len(nums)-1):
+            if nums[leng+1] - nums[leng] <= 1:
+                continue
+            else:
+                new.append([nums[leng]+1, nums[leng+1]-1])
+
+
+        if nums[-1] < upper:
+            new.append([(nums[-1])+1, upper])
 
         return new
