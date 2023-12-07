a87a070c277ac99577fcdff6f81b9c5d993263cb
Seoyoung Kim <idellio007@gmail.com>
2023-12-07 18:33:08 -0500

    Time: 927 ms (57.51%), Space: 0B (100.00%) - LeetHub

--- 0585-investments-in-2016/0585-investments-in-2016.sql
+++ 0585-investments-in-2016/0585-investments-in-2016.sql
@@ -14,15 +14,15 @@
 SELECT ROUND(SUM(tiv_2016), 2) AS tiv_2016
 FROM Insurance
 WHERE tiv_2015 in (
-    SELECT tiv_2015
-    FROM Insurance
-    GROUP BY tiv_2015
-    HAVING COUNT(tiv_2015) != 1
-    ) 
-    AND
-    (lat, lon) in (
-    SELECT lat, lon
-    FROM Insurance
-    GROUP BY lat, lon
-    HAVING COUNT(tiv_2015) = 1
-    )
+                    SELECT tiv_2015
+                    FROM Insurance
+                    GROUP BY tiv_2015
+                    HAVING COUNT(tiv_2015) != 1
+                    ) 
+                    AND
+                    (lat, lon) in (
+                    SELECT lat, lon
+                    FROM Insurance
+                    GROUP BY lat, lon
+                    HAVING COUNT(tiv_2015) = 1
+                    )
