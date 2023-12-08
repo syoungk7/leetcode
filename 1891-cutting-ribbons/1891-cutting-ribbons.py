18da291ee90de064f837f6756d42716513ec8edb
Seoyoung Kim <idellio007@gmail.com>
2023-12-07 21:52:28 -0500

    Time: 1004 ms (88.47%), Space: 30.8 MB (17.15%) - LeetHub

--- 1891-cutting-ribbons/1891-cutting-ribbons.py
+++ 1891-cutting-ribbons/1891-cutting-ribbons.py
@@ -1,18 +1,38 @@
 class Solution:
     def maxLength(self, ribbons: List[int], k: int) -> int:
-        left = 1
-        right = max(ribbons)
+#         left = 1
+#         right = max(ribbons)
         
-        while(left <= right):
-            mid = left + (right - left) // 2
-            num_of_ribbon = 0
+#         while(left <= right):
+#             mid = left + (right - left) // 2
+#             num_of_ribbon = 0
 
-            for i in ribbons:
-                num_of_ribbon += i // mid
+#             for i in ribbons:
+#                 num_of_ribbon += i // mid
 
-            if num_of_ribbon >= k:
-                left = mid + 1
+#             if num_of_ribbon >= k:
+#                 left = mid + 1
+#             else:
+#                 right = mid - 1
+
+#         return right
+    
+    
+        def f(n):
+            ans = 0
+            for ribbon in ribbons:
+                ans += ribbon//n
+            return ans
+
+        l = 1
+        r = sum(ribbons)//k+1
+
+        while l < r:
+            mid = l + (r-l)//2
+
+            if f(mid) < k:
+                r = mid
             else:
-                right = mid - 1
+                l = mid+1
 
-        return right
+        return l-1
