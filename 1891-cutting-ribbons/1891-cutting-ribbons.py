535d129b7ded58db98e7566e6e0d24ae32e39765
Seoyoung Kim <idellio007@gmail.com>
2023-12-07 21:29:42 -0500

    Time: 1169 ms (77.23%), Space: 30.5 MB (61.48%) - LeetHub

---
+++ 1891-cutting-ribbons/1891-cutting-ribbons.py
@@ -0,0 +1,18 @@
+class Solution:
+    def maxLength(self, ribbons: List[int], k: int) -> int:
+        start = 1
+        end = max(ribbons)
+        
+        while(start <= end):
+            mid = start + (end - start) // 2
+            res = 0
+
+            for i in ribbons:
+                res += i // mid
+
+            if res >= k:
+                start = mid+1
+            else:
+                end = mid -1
+
+        return end
