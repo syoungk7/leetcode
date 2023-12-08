25972847f0973527da7b01cce7ed079b7ffcb9dd
Seoyoung Kim <idellio007@gmail.com>
2023-12-07 21:53:22 -0500

    Time: 964 ms (92.68%), Space: 30.7 MB (17.15%) - LeetHub

--- 1891-cutting-ribbons/1891-cutting-ribbons.py
+++ 1891-cutting-ribbons/1891-cutting-ribbons.py
@@ -18,21 +18,21 @@ class Solution:
 #         return right
     
     
-        n = len(ribbons)
-        l, r = 1, max(ribbons)
+        def f(n):
+            ans = 0
+            for ribbon in ribbons:
+                ans += ribbon//n
+            return ans
 
-        def could(ribbons, length, k):
-            count = 0
-            for r in ribbons:
-                count += r//length
-                if count >= k: return True
-            return count >= k
-        
-        while l <= r:
-            m = l + (r-l)//2
-            if could(ribbons, m, k):
-                l = m+1
+        l = 1
+        r = sum(ribbons)//k+1
+
+        while l < r:
+            mid = l + (r-l)//2
+
+            if f(mid) < k:
+                r = mid
             else:
-                r = m-1
-        if r <= 0: return 0
-        return r
+                l = mid+1
+
+        return l-1
