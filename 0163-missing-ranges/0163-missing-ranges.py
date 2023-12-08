5995c2c14dc9379041c8f077cbffa6b1e49eea95
Seoyoung Kim <idellio007@gmail.com>
2023-12-07 21:46:13 -0500

    Time: 40 ms (41.59%), Space: 16.4 MB (24.46%) - LeetHub

--- 0163-missing-ranges/0163-missing-ranges.py
+++ 0163-missing-ranges/0163-missing-ranges.py
@@ -1,5 +1,27 @@
 class Solution:
     def findMissingRanges(self, nums: List[int], lower: int, upper: int) -> List[List[int]]:
+        new = []
+
+        if len(nums) == 0:
+            new.append([lower, upper])
+            return new
+
+        if nums[0] > lower:
+            new.append([lower, nums[0]-1])
+
+        for leng in range(0, len(nums)-1):
+            if nums[leng+1] - nums[leng] <= 1:
+                continue
+            else:
+                new.append([nums[leng]+1, nums[leng+1]-1])
+
+
+        if nums[-1] < upper:
+            new.append([(nums[-1])+1, upper])
+
+        return new
+    
+    
 #         new = []
 #         if len(nums) == 0:
 #             new.append([lower, upper])
@@ -14,24 +36,3 @@ class Solution:
 
 #         return new
     
-    
-        new = []
-        
-        if len(nums) == 0:
-            new.append([lower, upper])
-            return new
-
-        elif nums[0] > lower:
-            new.append([lower, nums[0]-1])
-
-        for leng in range(0, len(nums)-1):
-            if nums[leng+1] - nums[leng] <= 1:
-                continue
-            else:
-                new.append([nums[leng]+1, nums[leng+1]-1])
-
-
-        if nums[-1] < upper:
-            new.append([(nums[-1])+1, upper])
-
-        return new
