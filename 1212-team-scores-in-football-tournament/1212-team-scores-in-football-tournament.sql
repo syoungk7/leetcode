9893cf265f4174e82a76a5d9998c3e470a8ed086
Seoyoung Kim <idellio007@gmail.com>
2023-12-18 19:42:08 -0500

    Time: 1306 ms (83.85%), Space: 0B (100.00%) - LeetHub

--- 1212-team-scores-in-football-tournament/1212-team-scores-in-football-tournament.sql
+++ 1212-team-scores-in-football-tournament/1212-team-scores-in-football-tournament.sql
@@ -6,16 +6,10 @@ WITH Team_goals AS (
     UNION ALL
     SELECT guest_team AS team, guest_goals AS h_gals, host_goals AS g_goals
     FROM Matches)
-, Team_points AS (
-    SELECT team, SUM(IF(h_gals > g_goals, 3, IF(h_gals = g_goals, 1, 0))) AS points
-    FROM Team_goals
-    GROUP BY team
-    )
 
 SELECT team_id, team_name, IFNULL(t.points, 0) AS num_points
 FROM Teams
-LEFT JOIN Team_points t ON team_id = t.team
+LEFT JOIN (SELECT team, SUM(IF(h_gals > g_goals, 3, IF(h_gals = g_goals, 1, 0))) AS points
+           FROM Team_goals
+           GROUP BY team) t ON team_id = t.team
 ORDER BY num_points DESC, team_id ASC
-
-
- 
