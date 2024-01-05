f57980f3e768b67cbeed69eeeb6cdeb89cf72b30
Seoyoung Kim <idellio007@gmail.com>
2024-01-04 19:55:36 -0500

    Time: 39 ms (60.89%), Space: 17.1 MB (25.43%) - LeetHub

--- 1046-last-stone-weight/1046-last-stone-weight.py
+++ 1046-last-stone-weight/1046-last-stone-weight.py
@@ -4,13 +4,11 @@ class Solution:
         rev_stone = [-stone for stone in stones]
         heapq.heapify(rev_stone)
         
-        if len(rev_stone) != 1:
-            while len(rev_stone) != 1:
-                diff = heapq.heappop(rev_stone) - heapq.heappop(rev_stone)
-                heapq.heappush(rev_stone, diff)
-                # heapq.heappush(rev_stone, heapq.heappop(rev_stone) - heapq.heappop(rev_stone))
+        if len(rev_stone) == 0: return 0
 
-        if rev_stone:
-            return -rev_stone[0]
-        else:
-            return 0
+        while len(rev_stone) != 1:
+            diff = heapq.heappop(rev_stone) - heapq.heappop(rev_stone)
+            heapq.heappush(rev_stone, diff)
+
+        return -rev_stone[0]
+            
