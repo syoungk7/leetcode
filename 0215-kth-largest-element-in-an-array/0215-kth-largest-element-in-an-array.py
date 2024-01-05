d9ba4d5f0410de98e0fe2791420addd40a54dbc1
Seoyoung Kim <idellio007@gmail.com>
2024-01-04 22:01:37 -0500

    Time: 452 ms (97.14%), Space: 30.7 MB (23.28%) - LeetHub

--- 0215-kth-largest-element-in-an-array/0215-kth-largest-element-in-an-array.py
+++ 0215-kth-largest-element-in-an-array/0215-kth-largest-element-in-an-array.py
@@ -1,9 +1,11 @@
 class Solution:
     def findKthLargest(self, nums: List[int], k: int) -> int:
+        ## TC O(nlogn) / worst O(n^2)
+        ## SC O(1 or n/2) -> O(n)
         
-        if int(len(nums)/2) > k: 
-            return sorted(nums, reverse = True)[k-1]
-        elif int(len(nums)/2) == k: 
-            return sorted(nums, reverse = True)[k-1]
-        else: 
-            return sorted(nums, reverse = True)[int(len(nums)/2):k][-1]
+        # if int(len(nums)/2) > k: 
+        return sorted(nums, reverse = True)[k-1]
+        # elif int(len(nums)/2) == k: 
+        #     return sorted(nums, reverse = True)[k-1]
+        # else: 
+        #     return sorted(nums, reverse = True)[int(len(nums)/2):k][-1]
