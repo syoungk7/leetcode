e2832ec776b642421a895407e67fdd054273c1cc
Seoyoung Kim <idellio007@gmail.com>
2024-01-09 18:48:52 -0500

    Time: 5167 ms (5.08%), Space: 0B (100.00%) - LeetHub

--- 1511-customer-order-frequency/1511-customer-order-frequency.sql
+++ 1511-customer-order-frequency/1511-customer-order-frequency.sql
@@ -1,16 +1,11 @@
 # Write your MySQL query statement below
-WITH monthly_spending AS (SELECT customer_id,
-                            # MONTH(order_date) AS month, 
-                            SUM(price*quantity) AS spending
-                          FROM Orders o, Product p
-                          WHERE YEAR(order_date) = 2020 
-                            AND (MONTH(order_date) = 6 OR MONTH(order_date) = 7) 
-                            AND p.product_id = o.product_id
-                          GROUP BY customer_id, MONTH(order_date))
-
-SELECT c.customer_id, name
-FROM monthly_spending t, Customers c
-WHERE c.customer_id = t.customer_id
-GROUP BY c.customer_id
-HAVING SUM(IF(spending >= 100, 1, 0)) = 2
-    
+SELECT t.customer_id, t.name
+FROM (SELECT c.customer_id, name, 
+            # MONTH(order_date) AS month, 
+            IF(SUM(price*quantity)>=100, 1, 0) AS spending
+      FROM Customers c, Product p, Orders o
+      WHERE c.customer_id = o.customer_id AND p.product_id = o.product_id AND 
+            YEAR(order_date)=2020 AND (MONTH(order_date) = 6 OR MONTH(order_date) = 7)
+      GROUP BY c.customer_id, MONTH(order_date)) t
+GROUP BY t.customer_id
+HAVING SUM(spending) = 2
