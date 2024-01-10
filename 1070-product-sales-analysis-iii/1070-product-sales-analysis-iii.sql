7a2aa028d443981f019cbcc4e10cb2f66e80ca20
Seoyoung Kim <idellio007@gmail.com>
2024-01-10 18:55:06 -0500

    Time: 2427 ms (16.07%), Space: 0B (100.00%) - LeetHub

--- 1070-product-sales-analysis-iii/1070-product-sales-analysis-iii.sql
+++ 1070-product-sales-analysis-iii/1070-product-sales-analysis-iii.sql
@@ -1,4 +1,9 @@
 # Write your MySQL query statement below
+
+# SELECT product_id, MIN(year) AS first_year, quantity, price
+# FROM Sales
+# GROUP BY product_id
+
 SELECT product_id, year as first_year, quantity, price
 FROM Sales
 WHERE (product_id, year) in (SELECT product_id, MIN(year)
