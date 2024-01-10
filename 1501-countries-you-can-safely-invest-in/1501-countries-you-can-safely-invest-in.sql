a8b0819b69ecbbdffbe3c9fd7c015c09cf952b3c
Seoyoung Kim <idellio007@gmail.com>
2024-01-09 19:13:25 -0500

    Time: 2120 ms (50.65%), Space: 0B (100.00%) - LeetHub

--- 1501-countries-you-can-safely-invest-in/1501-countries-you-can-safely-invest-in.sql
+++ 1501-countries-you-can-safely-invest-in/1501-countries-you-can-safely-invest-in.sql
@@ -9,4 +9,5 @@ FROM (SELECT caller_id AS calls, duration
       Person p, Country c
 WHERE t.calls = p.id AND LEFT(p.phone_number, 3) = c.country_code
 GROUP BY c.name
-HAVING AVG(t.duration) >= (SELECT SUM(duration)/COUNT(2*caller_id) FROM Calls)
+# HAVING AVG(t.duration) >= (SELECT SUM(duration)/COUNT(2*caller_id) FROM Calls)
+HAVING AVG(t.duration) >= (SELECT AVG(duration) FROM Calls)
