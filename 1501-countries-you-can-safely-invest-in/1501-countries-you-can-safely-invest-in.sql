fe6f1221ecbb20e242edca70b0717dd8846fe167
Seoyoung Kim <idellio007@gmail.com>
2024-01-09 19:12:15 -0500

    Time: 2605 ms (17.91%), Space: 0B (100.00%) - LeetHub

---
+++ 1501-countries-you-can-safely-invest-in/1501-countries-you-can-safely-invest-in.sql
@@ -0,0 +1,12 @@
+# Write your MySQL query statement below
+
+SELECT c.name AS country
+FROM (SELECT caller_id AS calls, duration
+      FROM Calls
+      UNION
+      SELECT callee_id AS calls, duration
+      FROM Calls) t, 
+      Person p, Country c
+WHERE t.calls = p.id AND LEFT(p.phone_number, 3) = c.country_code
+GROUP BY c.name
+HAVING AVG(t.duration) >= (SELECT SUM(duration)/COUNT(2*caller_id) FROM Calls)
