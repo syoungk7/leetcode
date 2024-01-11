53206c827e3e6d97c245ba5fbc8ae3b5f4c0de22
Seoyoung Kim <idellio007@gmail.com>
2024-01-10 19:11:08 -0500

    Time: 1087 ms (49.02%), Space: 0B (100.00%) - LeetHub

--- 1045-customers-who-bought-all-products/1045-customers-who-bought-all-products.sql
+++ 1045-customers-who-bought-all-products/1045-customers-who-bought-all-products.sql
@@ -3,3 +3,4 @@ SELECT customer_id
 FROM Customer
 GROUP BY customer_id
 HAVING COUNT(DISTINCT product_key) = (SELECT COUNT(product_key) FROM Product)
+# HAVING SUM(IF(DISTINCT product_key IN (SELECT product_key FROM Product), 1, 0)) = (SELECT COUNT(product_key) FROM Product)
