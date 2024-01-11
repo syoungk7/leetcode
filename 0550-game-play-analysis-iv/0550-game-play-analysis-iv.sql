1586d5a4d8953c46e9c0db28cd59831351a8def8
Seoyoung Kim <idellio007@gmail.com>
2024-01-10 19:23:45 -0500

    Time: 998 ms (53.68%), Space: 0B (100.00%) - LeetHub

--- 0550-game-play-analysis-iv/0550-game-play-analysis-iv.sql
+++ 0550-game-play-analysis-iv/0550-game-play-analysis-iv.sql
@@ -1,26 +1,10 @@
 # Write your MySQL query statement below
-
-
-
-
-# WITH id_date AS (
-#     SELECT a.player_id, c.event_date
-#     FROM Activity a, Activity c
-#     WHERE a.player_id = c.player_id and DATEDIFF(a.event_date, min_date) = 1
-#     GROUP BY a.player_id, c.event_date)
-
-
-# SELECT ROUND(COUNT(player_id)/(SELECT COUNT(DISTINCT player_id) FROM Activity), 2) AS fraction
+# SELECT player_id, MIN(event_date) AS min_date
 # FROM Activity
-# WHERE (player_id, event_date - 1) in (SELECT player_id, MIN(event_date)
-#                                       FROM Activity
-#                                       GROUP BY player_id)
+# GROUP BY player_id
 
-# SELECT ROUND(num_player/(SELECT COUNT(DISTINCT player_id) 
-#                    FROM Activity), 2) AS fraction
-# FROM Activity a, selected s
-SELECT ROUND(COUNT(a.player_id)/(SELECT COUNT(DISTINCT player_id) FROM Activity), 2) AS fraction
-FROM Activity a, (SELECT player_id, MIN(event_date) as min_date
+SELECT ROUND(SUM(IF(DATEDIFF(a.event_date, t.min_date)=1, 1, 0))/COUNT(DISTINCT a.player_id), 2) AS fraction
+FROM Activity a, (SELECT player_id, MIN(event_date) AS min_date
                   FROM Activity
-                  GROUP BY player_id) s
-WHERE a.player_id = s.player_id and DATEDIFF(a.event_date, s.min_date) = 1
+                  GROUP BY player_id) t
+WHERE a.player_id = t.player_id
