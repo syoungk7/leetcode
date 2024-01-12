ba162d6b69f6049b14474c7fb237d6b5f79b6f0c
Seoyoung Kim <idellio007@gmail.com>
2024-01-11 19:34:36 -0500

    Time: 1136 ms (38.43%), Space: 0B (100.00%) - LeetHub

--- 1965-employees-with-missing-information/1965-employees-with-missing-information.sql
+++ 1965-employees-with-missing-information/1965-employees-with-missing-information.sql
@@ -3,7 +3,7 @@ SELECT t.employee_id
 FROM (SELECT s.employee_id, e.name, s.salary
       FROM Employees e
       RIGHT JOIN Salaries s ON e.employee_id = s.employee_id
-      UNION ALL
+      UNION
       SELECT e.employee_id, e.name, s.salary
       FROM Employees e
       LEFT JOIN Salaries s ON e.employee_id = s.employee_id) t
