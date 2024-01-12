ab29e6243bd8fbdb02db7be5501348be6d46996e
Seoyoung Kim <idellio007@gmail.com>
2024-01-11 19:34:26 -0500

    Time: 1037 ms (62.11%), Space: 0B (100.00%) - LeetHub

---
+++ 1965-employees-with-missing-information/1965-employees-with-missing-information.sql
@@ -0,0 +1,11 @@
+# Write your MySQL query statement below
+SELECT t.employee_id
+FROM (SELECT s.employee_id, e.name, s.salary
+      FROM Employees e
+      RIGHT JOIN Salaries s ON e.employee_id = s.employee_id
+      UNION ALL
+      SELECT e.employee_id, e.name, s.salary
+      FROM Employees e
+      LEFT JOIN Salaries s ON e.employee_id = s.employee_id) t
+WHERE t.name IS NULL or t.salary IS NULL
+ORDER BY t.employee_id
