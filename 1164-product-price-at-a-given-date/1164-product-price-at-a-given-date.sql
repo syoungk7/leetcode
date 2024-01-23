025daacfff50e1b4da18e4c53e547b1ca5a36ef2
Seoyoung Kim <idellio007@gmail.com>
2024-01-22 19:55:38 -0500

    Time: 2572 ms (5.00%), Space: 0B (100.00%) - LeetHub

--- 1164-product-price-at-a-given-date/1164-product-price-at-a-given-date.sql
+++ 1164-product-price-at-a-given-date/1164-product-price-at-a-given-date.sql
@@ -1,5 +1,4 @@
 # Write your MySQL query statement below
-
 SELECT DISTINCT p.product_id, IF(t.max_date IS NOT NULL, p.new_price, 10) AS price
 FROM Products p
 LEFT JOIN (SELECT product_id, MAX(change_date) as max_date
@@ -7,6 +6,4 @@ LEFT JOIN (SELECT product_id, MAX(change_date) as max_date
            WHERE change_date <= '2019-08-16'
            GROUP BY product_id) t 
     ON t.product_id = p.product_id
-
 WHERE p.change_date = t.max_date or t.max_date is null
-GROUP BY product_id
