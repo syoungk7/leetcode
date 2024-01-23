2e76c70082fcb98d7a67eeb1e9ae2b971e9f7c24
Seoyoung Kim <idellio007@gmail.com>
2024-01-22 20:05:56 -0500

    Time: 2529 ms (65.16%), Space: 0B (100.00%) - LeetHub

--- 1907-count-salary-categories/1907-count-salary-categories.sql
+++ 1907-count-salary-categories/1907-count-salary-categories.sql
@@ -1,27 +1,21 @@
 # Write your MySQL query statement below
-# WITH selected AS (
-#                 SELECT s.category, COUNT(s.category) AS accounts_count
-#                 FROM (
-#                     SELECT account_id,
-#                         CASE 
-#                             WHEN income < 20000 THEN 'Low Salary'
-#                             WHEN income <= 50000 THEN 'Average Salary'
-#                             ELSE 'High Salary'
-#                         END AS category
-#                     FROM Accounts
-#                     ) s
-#                 GROUP BY s.category
-#                 )
+# WITH selected AS (SELECT s.category, COUNT(s.category) AS accounts_count
+#                   FROM (SELECT account_id,
+#                             CASE 
+#                                 WHEN income < 20000 THEN 'Low Salary'
+#                                 WHEN income <= 50000 THEN 'Average Salary'
+#                                 ELSE 'High Salary'
+#                             END AS category
+#                         FROM Accounts) s
+#                 GROUP BY s.category)
 
 # SELECT n.category, IFNULL(s.accounts_count, 0) AS accounts_count
 # FROM selected s
-# RIGHT JOIN (
-#             SELECT 'Low Salary' as category
+# RIGHT JOIN (SELECT 'Low Salary' as category
 #             UNION
 #             SELECT 'Average Salary' as category
 #             UNION
-#             SELECT 'High Salary' as category
-#             ) n ON n.category = s.category
+#             SELECT 'High Salary' as category) n ON n.category = s.category
 
 
 # SELECT 'Low Salary' AS category,
@@ -36,18 +30,11 @@
 #   SUM(CASE WHEN income > 50000 THEN 1 ELSE 0 END) AS accounts_count
 # FROM  Accounts
 
-SELECT "Low Salary" AS category,
-       sum(income < 20000) AS accounts_count
-  FROM Accounts
-
+SELECT "Low Salary" AS category, SUM(income < 20000) AS accounts_count
+FROM Accounts
 UNION
-
-SELECT "Average Salary" AS category,
-       sum(income BETWEEN 20000 AND 50000) AS accounts_count
-  FROM Accounts
-
+SELECT "Average Salary" AS category, SUM(income BETWEEN 20000 AND 50000) AS accounts_count
+FROM Accounts
 UNION
-
-SELECT "High Salary" AS category,
-       sum(income > 50000) AS accounts_count
-  FROM Accounts;
+SELECT "High Salary" AS category, SUM(IF(income > 50000, 1, 0)) AS accounts_count
+FROM Accounts
