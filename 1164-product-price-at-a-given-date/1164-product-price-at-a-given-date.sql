30681fc1ecc5f395daee6d2d68316f551c5e87d0
Seoyoung Kim <idellio007@gmail.com>
2024-01-22 19:33:26 -0500

    Time: 878 ms (55.49%), Space: 0B (100.00%) - LeetHub

--- 1164-product-price-at-a-given-date/1164-product-price-at-a-given-date.sql
+++ 1164-product-price-at-a-given-date/1164-product-price-at-a-given-date.sql
@@ -1,31 +1,12 @@
 # Write your MySQL query statement below
-SELECT p.product_id, IF(s.max_date is null, 10, new_price) AS price
+
+SELECT DISTINCT p.product_id, IF(t.max_date IS NOT NULL, p.new_price, 10) AS price
 FROM Products p
+LEFT JOIN (SELECT product_id, MAX(change_date) as max_date
+           FROM Products
+           WHERE change_date <= '2019-08-16'
+           GROUP BY product_id) t 
+    ON t.product_id = p.product_id
 
-LEFT JOIN (
-    SELECT product_id, MAX(change_date) as max_date
-    FROM Products
-    WHERE change_date <= '2019-08-16'
-    GROUP BY product_id
-    ) s 
-    ON p.product_id = s.product_id
-
-WHERE p.change_date = s.max_date or s.max_date is null
-GROUP BY p.product_id
-
-# SELECT *, (rank() OVER price_rank) AS price_rank
-# FROM Products p
-# WINDOW price_rank AS (PARTITION BY product_id ORDER BY change_date DESC)
-
-# LEFT JOIN (
-    # SELECT product_id, new_price, change_date, (rank() OVER price_rank) AS price_rank
-    # FROM Products
-    # WHERE change_date <= '2019-08-16'
-    # WINDOW price_rank AS (PARTITION BY product_id ORDER BY change_date DESC)) s
-    # ON p.product_id = s.product_id
-
-    # SELECT product_id, change_date
-    #        FROM Products
-    #        #WHERE change_date <= '2019-08-16'
-    #        GROUP BY product_id 
-    #        HAVING MAX(change_date) >= change_date 
+WHERE p.change_date = t.max_date or t.max_date is null
+GROUP BY product_id
