2f73f7f88ac19569a8eff4e017115cb1fe31d7f9
Seoyoung Kim <idellio007@gmail.com>
2024-02-08 19:28:08 -0500

    Time: 710 ms (36.09%), Space: 0B (100.00%) - LeetHub

--- 1527-patients-with-a-condition/1527-patients-with-a-condition.sql
+++ 1527-patients-with-a-condition/1527-patients-with-a-condition.sql
@@ -1,9 +1,4 @@
-import pandas as pd
-
-def find_patients(patients: pd.DataFrame) -> pd.DataFrame:
-    #print(patients.apply(lambda x:  patients.conditions.str.split() if i.str.startswith('DIAB1')))
-    
-    return patients[patients['conditions'].str.startswith('DIAB1') |
-                    patients['conditions'].str.contains(' DIAB1')]
-
-# Series.str.split(pat=None, *, n=-1, expand=False, regex=None): Split strings around given separator/delimiter.
+# Write your MySQL query statement below
+SELECT patient_id, patient_name, conditions
+FROM Patients
+WHERE conditions LIKE 'DIAB1%' or conditions LIKE '% DIAB1%'
