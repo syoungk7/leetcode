724b8f6c5634d747f80d710c834453d41097b2c2
Seoyoung Kim <idellio007@gmail.com>
2024-02-09 18:26:35 -0500

    Time: 54 ms (30.70%), Space: 16.8 MB (51.94%) - LeetHub

---
+++ 0013-roman-to-integer/0013-roman-to-integer.py
@@ -0,0 +1,18 @@
+class Solution:
+    def romanToInt(self, s: str) -> int:
+        values = {'I': 1, 'V': 5, 'X': 10, 'L': 50, 'C': 100, 'D': 500, 'M': 1000}
+        values2 = {'IV': 4, 'IX': 9, 'XL': 40, 'XC': 90, 'CD': 400, 'CM': 900}
+        output = 0
+
+        for idx, sym in enumerate(s):
+            if s[idx:idx+1] and s[idx:idx+2] in values2:
+                print(output)
+                output += values2[s[idx:idx+2]]
+                print(output, s[idx:idx+2])
+                output -= values[s[idx+1]]
+                print(output, s[idx+1])
+            else:
+                output += values[sym]
+                print(output, idx, sym )
+    
+        return output
