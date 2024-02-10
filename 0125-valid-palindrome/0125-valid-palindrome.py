37a64363ed1d5c870361bf76c27d64c63741e77e
Seoyoung Kim <idellio007@gmail.com>
2024-02-09 21:21:21 -0500

    Time: 49 ms (45.60%), Space: 16.9 MB (96.91%) - LeetHub

--- 0125-valid-palindrome/0125-valid-palindrome.py
+++ 0125-valid-palindrome/0125-valid-palindrome.py
@@ -4,16 +4,18 @@ class Solution:
         # tmp = ''.join([char for char in s if char.isalnum()]).lower()
         # # print(tmp)
         # return tmp == tmp[::-1]
-        i, j = 0, len(s)-1
 
-        while i < j:
-            while i < j and not s[i].isalnum():
-                i+=1
-            while i < j and not s[j].isalnum():
-                j-=1
+        ## two points
+        left, right = 0, len(s)-1
 
-            if s[i].lower() != s[j].lower():
+        while left < right:
+            while left < right and not s[left].isalnum():
+                left += 1
+            while left < right and not s[right].isalnum():
+                right -= 1
+
+            if s[left].lower() != s[right].lower():
                 return False
-            i+=1
-            j-=1
+            left += 1
+            right -= 1
         return True
