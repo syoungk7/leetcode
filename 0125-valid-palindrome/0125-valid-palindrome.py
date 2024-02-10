47a83fb23506141dd83b2d05bcb429e06c2d016a
Seoyoung Kim <idellio007@gmail.com>
2024-02-09 21:53:11 -0500

    Time: 49 ms (45.60%), Space: 17 MB (96.91%) - LeetHub

--- 0125-valid-palindrome/0125-valid-palindrome.py
+++ 0125-valid-palindrome/0125-valid-palindrome.py
@@ -1,5 +1,6 @@
 class Solution:
     def isPalindrome(self, s: str) -> bool:
+        
         # #tmp = ''.join([char for char in s if char.isalpha() or char.isnumeric()]).lower()
         # tmp = ''.join([char for char in s if char.isalnum()]).lower()
         # # print(tmp)
@@ -9,13 +10,10 @@ class Solution:
         left, right = 0, len(s)-1
 
         while left < right:
-            while left < right and not s[left].isalnum():
-                left += 1
-            while left < right and not s[right].isalnum():
-                right -= 1
+            while left < right and not s[left].isalnum(): left += 1
+            while left < right and not s[right].isalnum(): right -= 1
+            if s[left].lower() != s[right].lower(): return False
 
-            if s[left].lower() != s[right].lower():
-                return False
             left += 1
             right -= 1
         return True
