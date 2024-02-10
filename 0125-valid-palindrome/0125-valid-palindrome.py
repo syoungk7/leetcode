67961d474a6e1e48f676807f8fe261539a8d8849
Seoyoung Kim <idellio007@gmail.com>
2024-02-09 22:14:43 -0500

    Time: 39 ms (89.06%), Space: 17 MB (85.22%) - LeetHub

--- 0125-valid-palindrome/0125-valid-palindrome.py
+++ 0125-valid-palindrome/0125-valid-palindrome.py
@@ -6,32 +6,38 @@ class Solution:
         # # print(tmp)
         # return tmp == tmp[::-1]
 
-#         ## two points
-#         left, right = 0, len(s)-1
+        ## two points
+        left, right = 0, len(s)-1
+
+        while left < right:
+            while left < right and not s[left].isalnum(): left += 1
+            while left < right and not s[right].isalnum(): right -= 1
+            if s[left].lower() != s[right].lower(): return False
+
+            left += 1
+            right -= 1
+        return True   
+
 #         regex = re.compile('[^a-zA-Z0-9]')
 
 #         while left < right:
 #             while left < right and not regex.sub('', s[left]): left += 1
 #             while left < right and not regex.sub('', s[right]): right -= 1
                                 
-# #             while left < right and not re.match('^[a-zA-Z0-9]', s[left]): left += 1
-# #             while left < right and not re.match('^[a-zA-Z0-9]', s[left]): right -= 1
-                
-#             if s[left].lower() != s[right].lower(): return False
+#             while left < right and not re.match('^[a-zA-Z0-9]', s[left]): left += 1
+#             while left < right and not re.match('^[a-zA-Z0-9]', s[left]): right -= 1
 
-#             left += 1
-#             right -= 1
+
+#         left, right = 0, len(s)-1
+#         while left < right:
+#             while left < right and not self.alphaNum(s[left]):
+#                 left += 1
+#             while right > left and not self.alphaNum(s[right]):
+#                 right -= 1
+#             if s[left].lower() != s[right].lower():
+#                 return False
+#             left, right = left+1, right-1
 #         return True
-        left, right = 0, len(s)-1
-        while left < right:
-            while left < right and not self.alphaNum(s[left]):
-                left += 1
-            while right > left and not self.alphaNum(s[right]):
-                right -= 1
-            if s[left].lower() != s[right].lower():
-                return False
-            left, right = left+1, right-1
-        return True
 
-    def alphaNum(self, c):
-        return (ord('0') <= ord(c) <= ord('9') or ord('a') <= ord(c) <= ord('z') or ord('A') <= ord(c) <= ord('Z'))
+#     def alphaNum(self, c):
+#         return (ord('0') <= ord(c) <= ord('9') or ord('a') <= ord(c) <= ord('z') or ord('A') <= ord(c) <= ord('Z'))
