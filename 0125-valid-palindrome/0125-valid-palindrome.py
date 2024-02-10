99fb3f93acebd48872c9c1d80ddc7718eb718e6a
Seoyoung Kim <idellio007@gmail.com>
2024-02-09 21:18:00 -0500

    Time: 43 ms (75.02%), Space: 22.7 MB (6.57%) - LeetHub

--- 0125-valid-palindrome/0125-valid-palindrome.py
+++ 0125-valid-palindrome/0125-valid-palindrome.py
@@ -1,7 +1,8 @@
 class Solution:
     def isPalindrome(self, s: str) -> bool:
-        #tmp = ''.join([char for char in s if char.isalpha() or char.isnumeric()]).lower()
-        tmp = ''.join([char for char in s if char.isalnum()]).lower()
-
-        print(tmp)
-        return tmp == tmp[::-1]
+        # #tmp = ''.join([char for char in s if char.isalpha() or char.isnumeric()]).lower()
+        # tmp = ''.join([char for char in s if char.isalnum()]).lower()
+        # print(tmp)
+        # return tmp == tmp[::-1]
+        s = [char.lower() for char in s if char.isalnum()]
+        return s == s[::-1]
