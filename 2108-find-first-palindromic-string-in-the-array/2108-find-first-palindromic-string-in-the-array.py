3f7553ae0dc9047ed4a216d3ece6ad724803b32a
Seoyoung Kim <idellio007@gmail.com>
2024-02-12 20:11:50 -0500

    Time: 92 ms (24.58%), Space: 16.6 MB (91.07%) - LeetHub

--- 2108-find-first-palindromic-string-in-the-array/2108-find-first-palindromic-string-in-the-array.py
+++ 2108-find-first-palindromic-string-in-the-array/2108-find-first-palindromic-string-in-the-array.py
@@ -1,8 +1,10 @@
 class Solution:
     def firstPalindrome(self, words: List[str]) -> str:
+        
         for word in words:
             length = len(word)
             left, right = 0, len(word)
+            
             while left < right:
                 if word[left] != word[right-1]:
                     break
@@ -10,6 +12,7 @@ class Solution:
                 right -= 1
             else: 
                 return word
+
         return ""
 #         for word in words:
 #             if word == word[::-1]:
