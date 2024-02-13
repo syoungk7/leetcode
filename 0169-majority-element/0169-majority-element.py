40477ecfa372a3bb68b408687e8b1df56bea3366
Seoyoung Kim <idellio007@gmail.com>
2024-02-12 20:20:22 -0500

    Time: 132 ms (76.69%), Space: 18.1 MB (63.93%) - LeetHub

--- 0169-majority-element/0169-majority-element.py
+++ 0169-majority-element/0169-majority-element.py
@@ -1,4 +1,17 @@
 class Solution:
     def majorityElement(self, nums: List[int]) -> int:
-        counts = collections.Counter(nums)
-        return max(counts.keys(), key=counts.get)
+        ## TC On, SC On
+        # counts = collections.Counter(nums)
+        # return max(counts.keys(), key=counts.get)
+    
+        # ## TC On^2, SC O1
+        # half = len(nums) // 2
+        # for num in nums:
+        #     counts = sum(1 for same in nums if num == same)
+        #     if counts > half:
+        #         return num
+        # time limit exceeded
+
+        ## TC Onlogn, SC O1
+        return sorted(nums)[len(nums) // 2]
+        
