104e110005fde31460189a855c9dbff9e85bd028
Seoyoung Kim <idellio007@gmail.com>
2024-02-12 20:12:13 -0500

    Time: 71 ms (86.93%), Space: 16.6 MB (81.18%) - LeetHub

--- 2108-find-first-palindromic-string-in-the-array/2108-find-first-palindromic-string-in-the-array.py
+++ 2108-find-first-palindromic-string-in-the-array/2108-find-first-palindromic-string-in-the-array.py
@@ -1,30 +1,23 @@
 class Solution:
     def firstPalindrome(self, words: List[str]) -> str:
         
-        for word in words:
-            length = len(word)
-            left, right = 0, len(word)
-            
-            while left < right:
-                if word[left] != word[right-1]:
-                    break
-                left += 1
-                right -= 1
-            else: 
-                return word
-
-        return ""
 #         for word in words:
-#             if word == word[::-1]:
+#             length = len(word)
+#             left, right = 0, len(word)
+            
+#             while left < right:
+#                 if word[left] != word[right-1]:
+#                     break
+#                 left += 1
+#                 right -= 1
+#             else: 
 #                 return word
 
 #         return ""
-    
-#         for word in words:
-#             i, j = 0, len(word) - 1
-#             while i < j:
-#                 if word[i] != word[j]: break
-#                 i += 1
-#                 j -= 1
-#             else: return word
-#         return ""
+
+
+
+        for word in words:
+            if word == word[::-1]:
+                return word
+        return ""
