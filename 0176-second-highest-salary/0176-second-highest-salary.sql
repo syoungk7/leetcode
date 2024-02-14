996e90f0e90f048f52fa72c3cc87477ae475e2f2
Seoyoung Kim <idellio007@gmail.com>
2024-02-13 19:45:01 -0500

    Time: 410 ms (87.32%), Space: 0B (100.00%) - LeetHub

--- 0176-second-highest-salary/0176-second-highest-salary.sql
+++ 0176-second-highest-salary/0176-second-highest-salary.sql
@@ -4,8 +4,14 @@
 # ORDER BY salary DESC 
 # LIMIT 1 OFFSET 1
 
-SELECT (SELECT DISTINCT salary
-                FROM Employee
-                GROUP BY salary
-                ORDER BY salary DESC
-                LIMIT 1 OFFSET 1) AS SecondHighestSalary
+# SELECT IFNULL((SELECT DISTINCT salary 
+    # FROM Employee
+    # GROUP BY salary
+    # ORDER BY salary DESC
+    # LIMIT 1 OFFSET 1), NULL) AS SecondHighestSalary
+
+SELECT (SELECT DISTINCT salary 
+    FROM Employee
+    GROUP BY salary
+    ORDER BY salary DESC
+    LIMIT 1 OFFSET 1) AS SecondHighestSalary
