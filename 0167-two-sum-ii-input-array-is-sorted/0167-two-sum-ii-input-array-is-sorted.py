e952503c790158bf2c4c85e3f31cfa3b756d8c66
Seoyoung Kim <idellio007@gmail.com>
2024-02-13 21:37:23 -0500

    Time: 107 ms (59.03%), Space: 17.5 MB (75.48%) - LeetHub

--- 0167-two-sum-ii-input-array-is-sorted/0167-two-sum-ii-input-array-is-sorted.py
+++ 0167-two-sum-ii-input-array-is-sorted/0167-two-sum-ii-input-array-is-sorted.py
@@ -1,18 +1,5 @@
 class Solution:
     def twoSum(self, numbers: List[int], target: int) -> List[int]:
-#         tmp_dic = defaultdict()
-        
-#         for i in range(len(numbers)):
-            
-#             if numbers[i] == target/2 and numbers[i] == numbers[i+1]:
-#                 return [i+1, i+2]
-            
-#             elif numbers[i] != target/2 and numbers[i] not in tmp_dic:
-#                 tmp_dic[numbers[i]] = i+1
-            
-#             if target - numbers[i] in tmp_dic:
-#                 return [tmp_dic[target - numbers[i]], i+1]
-            
         tmp_dic = {}
         
         for idx, val in enumerate(numbers):
@@ -20,4 +7,4 @@ class Solution:
             if target - val in tmp_dic: 
                 return [tmp_dic[target - val], idx + 1]
             
-            tmp_dic[val] = idx + 1
+            tmp_dic[val] = idx + 1        
