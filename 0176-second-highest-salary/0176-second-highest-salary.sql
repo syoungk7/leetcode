7211bfb86e3f8d0faa5f3e3e6a5f0412261a864d
Seoyoung Kim <idellio007@gmail.com>
2024-02-13 19:35:25 -0500

    Time: 442 ms (70.77%), Space: 0B (100.00%) - LeetHub

--- 0176-second-highest-salary/0176-second-highest-salary.sql
+++ 0176-second-highest-salary/0176-second-highest-salary.sql
@@ -1,6 +1,11 @@
 # Write your MySQL query statement below
-SELECT IFNULL((SELECT DISTINCT salary
+# SELECT DISTINCT salary AS SecondHighestSalary
+# FROM employee 
+# ORDER BY salary DESC 
+# LIMIT 1 OFFSET 1
+
+SELECT (SELECT DISTINCT salary
                 FROM Employee
                 GROUP BY salary
                 ORDER BY salary DESC
-                LIMIT 1 OFFSET 1), NULL) AS SecondHighestSalary
+                LIMIT 1 OFFSET 1) AS SecondHighestSalary
