dd9ed8fa955816e4c08dd3f200048003e3be2c8b
Seoyoung Kim <idellio007@gmail.com>
2024-02-14 19:48:43 -0500

    Time: 635 ms (57.05%), Space: 64.9 MB (42.50%) - LeetHub

--- 1795-rearrange-products-table/1795-rearrange-products-table.sql
+++ 1795-rearrange-products-table/1795-rearrange-products-table.sql
@@ -1,12 +1,14 @@
-# Write your MySQL query statement below
-SELECT product_id, 'store1' AS store, store1 AS price
-FROM Products
-WHERE store1 IS NOT NULL
-UNION
-SELECT product_id, 'store2' AS store, store2 AS price
-FROM Products
-WHERE store2 IS NOT NULL
-UNION
-SELECT product_id, 'store3' AS store, store3 AS price
-FROM Products
-WHERE store3 IS NOT NULL
+import pandas as pd
+
+def rearrange_products_table(products: pd.DataFrame) -> pd.DataFrame:
+    df = products.melt(id_vars=['product_id'], 
+                       var_name='store', 
+                       value_name='price')
+    return df.dropna(subset='price')
+    
+    
+# df1 = (df.set_index(["location", "name"])
+#          .stack()
+#          .reset_index(name='Value')
+#          .rename(columns={'level_2':'Date'}))
+## DataFrame.dropna(*, axis=0, how=_NoDefault.no_default, thresh=_NoDefault.no_default, subset=None, inplace=False, ignore_index=False)
