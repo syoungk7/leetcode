31644e7cd1a88cd052e6826ca982d0fc4a48c79b
Seoyoung Kim <idellio007@gmail.com>
2024-02-14 19:36:18 -0500

    Time: 1209 ms (89.14%), Space: 0B (100.00%) - LeetHub

--- 0196-delete-duplicate-emails/0196-delete-duplicate-emails.sql
+++ 0196-delete-duplicate-emails/0196-delete-duplicate-emails.sql
@@ -1,9 +1,9 @@
 # Write your MySQL query statement below
 DELETE 
 FROM person 
-WHERE (id, email) NOT IN (SELECT id, email
-                          FROM (SELECT MIN(id) AS id, email
+# WHERE (id, email) NOT IN (SELECT id, email
+#                           FROM (SELECT MIN(id) AS id, email
+WHERE id NOT IN (SELECT id
+                          FROM (SELECT MIN(id) AS id
                                 FROM person
                                 GROUP BY email) t)
-
-# delete from Person where id not in (select id from (select min(id) as id from Person group by email) as derived_table);
