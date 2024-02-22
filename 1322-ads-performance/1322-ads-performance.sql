df8bcde2556069c57395b3318013b88d0e723c8d
Seoyoung Kim <idellio007@gmail.com>
2024-02-21 19:56:19 -0500

    Time: 964 ms (69.05%), Space: 0B (100.00%) - LeetHub

---
+++ 1322-ads-performance/1322-ads-performance.sql
@@ -0,0 +1,6 @@
+# Write your MySQL query statement below
+SELECT ad_id, 
+    IFNULL(ROUND(SUM(IF(action = 'Clicked', 1, 0 )) / SUM(IF(action in ('Clicked', 'Viewed'), 1, 0)) * 100, 2), 0) as ctr
+FROM ads
+GROUP BY ad_id
+ORDER BY ctr DESC, ad_id 
