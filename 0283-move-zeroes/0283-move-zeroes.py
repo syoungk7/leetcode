941aa71fe30bb9521ca0be0c480538ca630acfc9
Seoyoung Kim <idellio007@gmail.com>
2024-04-22 11:55:05 -0400

    Time: 129 ms (48.95%), Space: 18.2 MB (31.25%) - LeetHub

--- 0283-move-zeroes/0283-move-zeroes.py
+++ 0283-move-zeroes/0283-move-zeroes.py
@@ -3,22 +3,9 @@ class Solution:
         """
         Do not return anything, modify nums in-place instead.
         """
-        # # nums = sorted(nums)
-        # for i in range(1, len(nums)):
-        #     if nums[i-1] == 0 or num[i-1] > nums[i]:
-        #         nums[i-1], nums[i] = nums[i], nums[i-1]
-        #     print(nums) 
-#         left = 0
-
-#         for right in range(len(nums)):
-#             if nums[right] != 0:
-#                 nums[right], nums[left] = nums[left], nums[right]
-#                 left += 1
-#             print(nums)
-        
-#         return nums
         left = 0
-        for right in range(len(nums)):
+
+        for right in range(1, len(nums)):
             if nums[right] != 0 and nums[left] == 0:
                 nums[left], nums[right] = nums[right], nums[left]
 
