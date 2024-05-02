810367aa3ea62c1396e71ea64efa4c85d0009505
Seoyoung Kim <idellio007@gmail.com>
2024-05-02 00:24:21 -0400

    Time: 121 ms (82.69%), Space: 18.2 MB (31.25%) - LeetHub

--- 0283-move-zeroes/0283-move-zeroes.py
+++ 0283-move-zeroes/0283-move-zeroes.py
@@ -3,13 +3,21 @@ class Solution:
         """
         Do not return anything, modify nums in-place instead.
         """
-        left = 0
+#         left = 0
 
-        for right in range(1, len(nums)):
-            if nums[right] != 0 and nums[left] == 0:
-                nums[left], nums[right] = nums[right], nums[left]
+#         for right in range(1, len(nums)):
+#             if nums[right] != 0 and nums[left] == 0:
+#                 nums[left], nums[right] = nums[right], nums[left]
 
-            # wait while we find a non-zero element to
-            # swap with you
-            if nums[left] != 0:
-                left += 1
+#             # wait while we find a non-zero element to swap with you
+#             if nums[left] != 0:
+#                 left += 1
+        
+        count = 0
+        for i in nums:
+            if i != 0:
+                nums[count] = i
+                count += 1
+        
+        for i in range(count, len(nums)):
+            nums[i] = 0
