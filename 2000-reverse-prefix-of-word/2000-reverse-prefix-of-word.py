f3efdcbd516a11df9f8b774cf137c477ad070a22
Seoyoung Kim <idellio007@gmail.com>
2024-05-01 23:28:34 -0400

    Time: 24 ms (97.63%), Space: 16.5 MB (46.76%) - LeetHub

--- 2000-reverse-prefix-of-word/2000-reverse-prefix-of-word.py
+++ 2000-reverse-prefix-of-word/2000-reverse-prefix-of-word.py
@@ -1,14 +1,14 @@
-# class Solution:
-#     def reversePrefix(self, word: str, ch: str) -> str:
-#         if ch in word:
-#             rev_idx = word.index(ch)+1
-#             return word[0:rev_idx][::-1] + word[rev_idx::]
-#         else:
-#             return word
 class Solution:
     def reversePrefix(self, word: str, ch: str) -> str:
         if ch not in word: return word
+
+        rev_idx = word.index(ch)
+        return word[rev_idx::-1] + word[rev_idx+1::]
+           
+# class Solution:
+#     def reversePrefix(self, word: str, ch: str) -> str:
+#         if ch not in word: return word
     
-        i = word.index(ch)
-        z = word[i::-1]
-        return z + word[i+1::]
+#         i = word.index(ch)
+#         z = word[i::-1]
+#         return z + word[i+1::]
