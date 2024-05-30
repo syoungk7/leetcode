bc690b78d674a54a621d15d0cee8ce3ffa30671a
Seoyoung Kim <idellio007@gmail.com>
2024-05-30 15:02:53 -0400

    Time: 49 ms (80.96%), Space: 16.7 MB (82.17%) - LeetHub

--- 0383-ransom-note/0383-ransom-note.py
+++ 0383-ransom-note/0383-ransom-note.py
@@ -1,10 +1,21 @@
 class Solution:
     def canConstruct(self, ransomNote: str, magazine: str) -> bool:
-        tmp = [i for i in magazine]
+#         tmp = [i for i in magazine]
 
-        for i in ransomNote:
-            if i in tmp:
-                tmp.pop(tmp.index(i))
-            else:
+#         for i in ransomNote:
+#             if i in tmp:
+#                 tmp.pop(tmp.index(i))
+#             else:
+#                 return False
+#         return True
+
+        if len(ransomNote) > len(magazine): return False
+
+        magazine_counts = collections.Counter(magazine)
+        ransom_note_counts = collections.Counter(ransomNote)
+
+        for char, count in ransom_note_counts.items():
+            magazine_count = magazine_counts[char]
+            if magazine_count < count:
                 return False
         return True
