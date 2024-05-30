671a6e28e830e531e99d367f5ae37611ef01c309
Seoyoung Kim <idellio007@gmail.com>
2024-05-30 14:59:12 -0400

    Time: 58 ms (54.03%), Space: 17 MB (8.93%) - LeetHub

--- 0383-ransom-note/0383-ransom-note.py
+++ 0383-ransom-note/0383-ransom-note.py
@@ -1,9 +1,8 @@
 class Solution:
     def canConstruct(self, ransomNote: str, magazine: str) -> bool:
         tmp = [i for i in magazine]
-        print(tmp)
+
         for i in ransomNote:
-            
             if i in tmp:
                 tmp.pop(tmp.index(i))
             else:
