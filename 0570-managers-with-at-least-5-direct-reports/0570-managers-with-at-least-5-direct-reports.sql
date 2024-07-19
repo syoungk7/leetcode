7bc72ac7037f1f8e2963884994ba4d4e48638f62
Seoyoung Kim <idellio007@gmail.com>
2024-07-18 22:46:00 -0400

    Time: 325 ms (73.68%), Space: 0B (100.00%) - LeetHub

--- 0570-managers-with-at-least-5-direct-reports/0570-managers-with-at-least-5-direct-reports.sql
+++ 0570-managers-with-at-least-5-direct-reports/0570-managers-with-at-least-5-direct-reports.sql
@@ -1,7 +1,8 @@
 # Write your MySQL query statement below
-SELECT my.name
-FROM
-    (SELECT e2.name, COUNT(e1.managerId) as n
-    FROM Employee e1 JOIN Employee e2 ON e1.managerId = e2.id
-    GROUP BY e1.managerId) my
-WHERE n >= 5
+SELECT name
+FROM Employee
+WHERE id in (
+SELECT managerId
+FROM Employee
+GROUP BY managerId
+HAVING count(id) >= 5);
