af683dfb601c728334a59fd9b319813c0fa7476c
Seoyoung Kim <idellio007@gmail.com>
2024-07-18 23:06:16 -0400

    Time: 578 ms (79.96%), Space: 0B (100.00%) - LeetHub

--- 1934-confirmation-rate/1934-confirmation-rate.sql
+++ 1934-confirmation-rate/1934-confirmation-rate.sql
@@ -1,11 +1,8 @@
 # Write your MySQL query statement below
-SELECT s.user_id, COALESCE(ROUND(AVG(actnum), 2), 0) AS confirmation_rate
-FROM Signups s
-LEFT JOIN (SELECT *,
-                CASE
-                    WHEN action = 'timeout' THEN 0
-                    WHEN action = 'confirmed' THEN 1
-                END AS actnum
-            FROM Confirmations) as my
-ON s.user_id = my.user_id
-GROUP BY s.user_id
+select s.user_id,
+round(ifnull(sum(case when c.action = 'confirmed' then 1 else 0 end) /
+count(c.user_id), 0), 2) as confirmation_rate
+from signups s
+left join Confirmations c
+on s.user_id = c.user_id
+group by s.user_id
