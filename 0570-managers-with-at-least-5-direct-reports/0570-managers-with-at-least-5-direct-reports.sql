7fe5ac932bc3b11fe4f8b1b12cf8e49f1e67cbc4
Seoyoung Kim <idellio007@gmail.com>
2024-07-18 22:45:27 -0400

    Time: 349 ms (56.27%), Space: 0B (100.00%) - LeetHub

--- 0570-managers-with-at-least-5-direct-reports/0570-managers-with-at-least-5-direct-reports.sql
+++ 0570-managers-with-at-least-5-direct-reports/0570-managers-with-at-least-5-direct-reports.sql
@@ -1,7 +1,7 @@
 # Write your MySQL query statement below
-select e.name from employee e
-where e.id in
-(select managerId from employee
-where managerId is not null
+select e1.name
+from employee as e1, (select managerId
+from employee
 group by managerId
-having count(*) >= 5)
+having count(*) >= 5) as e2
+where e1.id = e2.managerId
