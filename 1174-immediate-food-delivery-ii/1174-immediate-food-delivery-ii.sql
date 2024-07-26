b0ea8c90cbce60959075a8d8e472e6a60f972478
Seoyoung Kim <idellio007@gmail.com>
2024-07-25 21:59:01 -0400

    Time: 1157 ms (16.15%), Space: 0B (100.00%) - LeetHub

--- 1174-immediate-food-delivery-ii/1174-immediate-food-delivery-ii.sql
+++ 1174-immediate-food-delivery-ii/1174-immediate-food-delivery-ii.sql
@@ -1,4 +1,5 @@
-select round(100*(sum(case when order_date = customer_pref_delivery_date then 1 else 0 end)/count(distinct customer_id)), 2) as immediate_percentage
-from (select *, row_number() over(partition by customer_id order by order_date) as row_num
-from delivery) as grouped_order
-where row_num = 1
+select round(count(d1.customer_id) / (select count(distinct d3.customer_id) from Delivery d3) * 100, 2) as immediate_percentage
+from Delivery d1
+where d1.order_date =
+    (select min(d2.order_date) from Delivery d2 where d1.customer_id = d2.customer_id)
+    and d1.order_date = d1.customer_pref_delivery_date
