b8d9c12a0493ffe6afd46ae529e7731d05bcab8d
Seoyoung Kim <idellio007@gmail.com>
2024-07-25 22:36:03 -0400

    Time: 294 ms (97.33%), Space: 0B (100.00%) - LeetHub

--- 1211-queries-quality-and-percentage/1211-queries-quality-and-percentage.sql
+++ 1211-queries-quality-and-percentage/1211-queries-quality-and-percentage.sql
@@ -1,6 +1,10 @@
-select query_name,
-        round((sum(rating/position)/count(*)), 2) as quality,
-        round(100 * (sum(case when rating < 3 then 1 else 0 end)/count(*)), 2) as poor_query_percentage
-from queries
-where query_name is not null
-group by query_name
+with query_stats as (
+    select query_name,
+    avg(rating * 1.0 / position) as quality,
+    sum(CASE when rating < 3 then 1 else 0 end) * 100.0 / count(*) AS poor_query_percentage
+    from queries
+    where query_name is not null
+    group by query_name)
+
+select query_name, round(quality, 2) as quality, round(poor_query_percentage, 2) as poor_query_percentage
+from query_stats
