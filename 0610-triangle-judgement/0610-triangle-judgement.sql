320ab916495cd2adb53c58e52aa3aa73c9a546fb
Seoyoung Kim <idellio007@gmail.com>
2024-08-06 01:06:33 -0400

    Time: 1615 ms (5.00%), Space: 0B (100.00%) - LeetHub

--- 0610-triangle-judgement/0610-triangle-judgement.sql
+++ 0610-triangle-judgement/0610-triangle-judgement.sql
@@ -1,18 +1,3 @@
 # Write your MySQL query statement below
-
-# SELECT t.x, t.y, t.z, IF(t.x = s.x and s.triangle = 1, 'Yes', 'No') AS triangle
-# FROM Triangle t, (SELECT x, y, z, 1 AS triangle
-#                   FROM Triangle
-#                   WHERE x + y > z and x + z > y and y + z > x) s
-
-# SELECT t.x, t.y, t.z, IF(s.triangle is null, 'No', 'Yes') AS triangle
-# FROM Triangle t
-# LEFT JOIN (SELECT x, y, z, 1 AS triangle
-#            FROM Triangle
-#            WHERE x + y > z and x + z > y and y + z > x) s on t.x = s.x 
-
-# SELECT x, y, z, CASE WHEN (x + y > z and x + z > y and y + z > x) THEN 'Yes' ELSE 'No' END AS triangle
-# FROM Triangle
-
-SELECT x, y, z, IF((x + y > z and x + z > y and y + z > x), 'Yes', 'No') AS triangle 
+SELECT x, y, z, IF((x + y > z and x + z > y and y + z > x), 'Yes', 'No') AS triangle
 FROM Triangle
