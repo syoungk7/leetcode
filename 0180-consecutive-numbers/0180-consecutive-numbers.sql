3f31625d6dffde52fbc2c1641b46a217d4bb951f
Seoyoung Kim <idellio007@gmail.com>
2024-08-08 23:09:51 -0400

    Time: 518 ms (56.42%), Space: 0B (100.00%) - LeetHub

--- 0180-consecutive-numbers/0180-consecutive-numbers.sql
+++ 0180-consecutive-numbers/0180-consecutive-numbers.sql
@@ -1,10 +1,45 @@
 # Write your MySQL query statement below
-# SELECT  DISTINCT (l.num) AS ConsecutiveNums
-# FROM Logs l, logs o, logs g
-# WHERE l.id = o.id + 1 AND o.id = g.id + 1 AND 
-#     (l.num = o.num AND o.num = g.num)
-    
-SELECT DISTINCT l1.num as ConsecutiveNums
-FROM Logs l1 
-JOIN Logs l2 ON (l1.id = l2.id-1 AND l1.num=l2.num)
-JOIN Logs l3 ON (l2.id = l3.id-1 AND l2.num=l3.num)
+
+# select distinct num as ConsecutiveNums
+# from (select id-1, num,
+#         lag(num) over (order by id) as prev_num,
+#         lead (num) over (order by id) as next_num from logs) as sub
+# where num = prev_num and prev_num = next_num
+
+
+# select *,
+#     lag(num) over (order by id) as prev_num,
+#     lead (num) over (order by id) as next_num 
+# from logs
+# where 
+
+with CTE as (
+    select
+        num,
+        id,
+        lead(num, 1)  over (order by id) num1,
+        lead(num, 2)  over (order by id) num2,
+        lead(id, 1) over (order by id) id1,
+        lead(id, 2) over (order by id) id2
+    from logs
+)
+
+select distinct num as "ConsecutiveNums" from cte
+where num = num1 and num = num2 and id = id1 - 1 and id = id2 -2;
+
+
+# With test as (
+#     select *, row_number() over(order by id) as row_num
+#     from Logs)
+
+# select distinct num as ConsecutiveNums
+# from (select num,
+#     lag(num) over (order by row_num) as prev_num,
+#     lead(num) over (order by row_num) as next_num from test) as sub
+# where num = prev_num and prev_num = next_num and 
+
+
+# select num,
+# lag(num) over (order by row_num) as prev_num,
+# lead (num) over (order by row_num) as next_num 
+# from test
